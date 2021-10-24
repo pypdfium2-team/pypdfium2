44d6d7a5b5d9c9c4f5286d0689a463e63ace285a
mara004 <geisserml@gmail.com>
2021-10-24 10:28:34 +0200

    _version.py: use .format() rather than f-strings
    This allows us to lower the required target Python version

--- src/pypdfium2/_version.py
+++ src/pypdfium2/_version.py
@@ -6,8 +6,8 @@ V_MINOR = 1
 V_PATCH = 0
 V_BETA  = 0
 
-V_PYPDFIUM2 = f"{V_MAJOR}.{V_MINOR}.{V_PATCH}"
+V_PYPDFIUM2 = "{}.{}.{}".format(V_MAJOR, V_MINOR, V_PATCH)
 if V_BETA is not None:
-    V_PYPDFIUM2 += f'b{V_BETA}'
+    V_PYPDFIUM2 += "b{}".format(V_BETA)
 
 V_LIBPDFIUM = 4678
