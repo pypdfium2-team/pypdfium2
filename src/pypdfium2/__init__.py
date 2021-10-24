bf1e90f599e0ad0c34ed3a0b7f15986e7ed72c65
Adam Huganir <adam@huganir.com>
2021-10-24 16:19:07 -0400

    Poetry dependencies and dev dependencies added. Stub for cli, updated config files with some settings for tooling. Removed python 3.5 support since it is significantly deprecated

--- src/pypdfium2/__init__.py
+++ src/pypdfium2/__init__.py
@@ -1,8 +1,9 @@
 # SPDX-FileCopyrightText: 2021 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0
 
-from pypdfium2 import _version
-from pypdfium2._pypdfium import *
+from . import _version
+from ._pypdfium import *
+
 
 __version__ = _version.V_PYPDFIUM2
 __pdfium_version__ = _version.V_LIBPDFIUM
---
+++ src/pypdfium2/cli/__init__.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+# SPDX-FileCopyrightText: 2021 geisserml <geisserml@gmail.com>
+# SPDX-FileCopyrightText: 2020 Yinlin Hu <huyinlin@gmail.com>
+# SPDX-License-Identifier: CC-BY-4.0
+
+import sys
+import ctypes
+from PIL import Image
+import pypdfium2 as pdfium
+
+pdfium.FPDF_InitLibraryWithConfig(None)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage: example.py somefile.pdf")
+        sys.exit()
+
+    filename = sys.argv[1]
+
+    doc = pdfium.FPDF_LoadDocument(filename, None) # load document (filename, password string)
+    page_count = pdfium.FPDF_GetPageCount(doc)     # get page count
+    assert page_count >= 1
+
+    page   = pdfium.FPDF_LoadPage(doc, 0)                # load the first page
+    width  = int(pdfium.FPDF_GetPageWidthF(page)  + 0.5) # get page width
+    height = int(pdfium.FPDF_GetPageHeightF(page) + 0.5) # get page height
+
+    # render to bitmap
+    bitmap = pdfium.FPDFBitmap_Create(width, height, 0)
+    pdfium.FPDFBitmap_FillRect(bitmap, 0, 0, width, height, 0xFFFFFFFF)
+    pdfium.FPDF_RenderPageBitmap(
+        bitmap, page, 0, 0, width, height, 0,
+        pdfium.FPDF_LCD_TEXT | pdfium.FPDF_ANNOT
+    )
+
+    # retrieve data from bitmap
+    cbuffer = pdfium.FPDFBitmap_GetBuffer(bitmap)
+    buffer = ctypes.cast(cbuffer, ctypes.POINTER(ctypes.c_ubyte * (width * height * 4)))
+
+    img = Image.frombuffer("RGBA", (width, height), buffer.contents, "raw", "BGRA", 0, 1)
+    img.save("out.png")
+
+    if bitmap is not None:
+        pdfium.FPDFBitmap_Destroy(bitmap)
+    pdfium.FPDF_ClosePage(page)
+
+    pdfium.FPDF_CloseDocument(doc)
