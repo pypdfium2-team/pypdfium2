0fc0c27c2d03908634728c25ec2635b90c12cfc0
mara004 <geisserml@gmail.com>
2021-11-19 14:36:35 +0100

    init: install an exit handler to call `FPDF_DestroyLibrary()`

--- src/pypdfium2/__init__.py
+++ src/pypdfium2/__init__.py
@@ -2,17 +2,24 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import logging
+from pypdfium2._logging import setup_logger
+logger = logging.getLogger(__name__)
+setup_logger(logger)
+
+import atexit
 from pypdfium2 import _version
 from pypdfium2._pypdfium import *
 from pypdfium2._helpers import *
 from pypdfium2._constants import *
 from pypdfium2._exceptions import *
-from pypdfium2._logging import setup_logger
 
 __version__ = _version.V_PYPDFIUM2
 __pdfium_version__ = _version.V_LIBPDFIUM
 
-logger = logging.getLogger(__name__)
-setup_logger(logger)
 
 FPDF_InitLibrary()
+
+def exit_handler():
+    FPDF_DestroyLibrary()
+
+atexit.register(exit_handler)
