780bf97bdddfe458b319e83e0b0b825700827662
Adam Huganir <adam@huganir.com>
2021-11-30 10:19:36 -0500

    removed modified _version

--- src/pypdfium2/_version.py
+++ src/pypdfium2/_version.py
@@ -2,12 +2,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
 V_MAJOR = 0
-V_MINOR = 2
+V_MINOR = 1
 V_PATCH = 0
-V_BETA = 0
+V_BETA  = 0
 
 V_PYPDFIUM2 = "{}.{}.{}".format(V_MAJOR, V_MINOR, V_PATCH)
 if V_BETA is not None:
     V_PYPDFIUM2 += "b{}".format(V_BETA)
 
-V_LIBPDFIUM = 4736
+V_LIBPDFIUM = 4678
