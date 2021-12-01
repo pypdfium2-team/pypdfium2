73ccd1b4058c4fa1e0572b3476affab9bed8b815
Adam Huganir <adam@huganir.com>
2021-12-01 07:13:03 -0500

    Add preliminary tests for the command-line interface (#17)
    Authored by @adam-huganir
    
    rename cli test file

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2021 Adam Huganir <adam@huganir.com>
+# SPDX-FileCopyrightText: 2021 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
---
+++ tests/test_main.py
@@ -0,0 +1,85 @@
+# SPDX-FileCopyrightText: 2021 Adam Huganir <adam@huganir.com>
+# SPDX-FileCopyrightText: 2021 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+from pypdfium2 import __main__ as main
+from pypdfium2._constants import OptimiseMode
+import pytest
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("0", 0),
+        ("90", 90),
+        ("180", 180),
+        ("270", 270),
+    ],
+)
+def test_rotation_type(test_input, expected):
+    assert main.rotation_type(test_input) == expected
+
+
+def test_rotation_type_fail_oob():
+    with pytest.raises(ValueError):
+        main.rotation_type("101")
+    with pytest.raises(ValueError):
+        main.rotation_type("string")
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("None", None),
+        ("none", None),
+        ("10", 10),
+        ("0xFFFFFFFF", 0xFFFFFFFF),
+    ],
+)
+def test_hex_or_none_type(test_input, expected):
+    assert main.hex_or_none_type(test_input) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("", None),
+        ("1,2", [0, 1]),
+        ("3-5", [2, 3, 4]),
+        ("5-3", [4, 3, 2]),
+        ("1", [0]),
+    ],
+)
+def test_pagetext_type(test_input, expected):
+    assert main.pagetext_type(test_input) == expected
+
+
+def test_parse_args():
+    
+    argv = [
+        '-i', 'path/to/document.pdf',
+        '-o', 'output_dir/',
+        '--password', 'test-password',
+        '--prefix', 'render_',
+        '--pages', '1,4,5-7,6-4',
+        '--scale', '2',
+        '--rotation', '90',
+        '--background-colour', '0xFFFFFFFF',
+        '--optimise-mode', 'none',
+        '--processes', '4',
+    ]
+    
+    args = main.parse_args(argv)
+    
+    assert args.pdffile == 'path/to/document.pdf'
+    assert args.output == 'output_dir/'
+    assert args.password == 'test-password'
+    assert args.prefix == 'render_'
+    assert args.pages == [0, 3, 4, 5, 6, 5, 4, 3]
+    assert args.scale == 2
+    assert args.rotation == 90
+    assert args.background_colour == 0xFFFFFFFF
+    assert args.no_annotations == False
+    assert args.optimise_mode == OptimiseMode.none
+    assert args.processes == 4
+    assert args.version == False
