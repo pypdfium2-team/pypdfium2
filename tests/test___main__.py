c9dd03d769e5f873f5deb9718e41c1958c60316c
geisserml <geisserml@gmail.com>
2021-12-01 12:54:40 +0100

    add myself to the spdx header

--- tests/test___main__.py
+++ tests/test___main__.py
@@ -1,4 +1,5 @@
 # SPDX-FileCopyrightText: 2021 Adam Huganir <adam@huganir.com>
+# SPDX-FileCopyrightText: 2021 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0
 
 from pypdfium2 import OptimiseMode
