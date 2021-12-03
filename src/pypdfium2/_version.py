c51dbb47dfda033a308b2e3e2be77838bf0e6129
mara004 <geisserml@gmail.com>
2021-12-03 16:08:17 +0100

    Set version for first release

--- src/pypdfium2/_version.py
+++ src/pypdfium2/_version.py
@@ -4,10 +4,10 @@
 V_MAJOR = 0
 V_MINOR = 1
 V_PATCH = 0
-V_BETA  = 0
+V_BETA  = None
 
 V_PYPDFIUM2 = "{}.{}.{}".format(V_MAJOR, V_MINOR, V_PATCH)
 if V_BETA is not None:
     V_PYPDFIUM2 += "b{}".format(V_BETA)
 
-V_LIBPDFIUM = 4678
+V_LIBPDFIUM = 4736
