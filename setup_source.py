83739b93c03a375725bab9f6e89c8f3d05a977cd
mara004 <geisserml@gmail.com>
2021-12-04 16:23:29 +0100

    clean after run

--- setup_source.py
+++ setup_source.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: 2021 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0
 
+import os
 from setup_base import *
 from wheel.bdist_wheel import bdist_wheel
 from sourcebuild import main as sourcebuild_main
@@ -15,5 +16,9 @@ def lib_setup(libname):
 
 
 if __name__ == '__main__':
+    
     libname = sourcebuild_main()
     build(lambda: lib_setup(libname), SourceBuild)
+    
+    binary = join(SourceTree,'src','pypdfium2',libname)
+    os.remove(binary)
