6cf8507cf7535efd6a43f6b6aa7f5c8da835a695
mara004 <geisserml@gmail.com>
2021-12-04 16:16:28 +0100

    Very experimental source build support

---
+++ setup_source.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# SPDX-FileCopyrightText: 2021 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+from setup_base import *
+from wheel.bdist_wheel import bdist_wheel
+from sourcebuild import main as sourcebuild_main
+
+
+def lib_setup(libname):
+    setuptools.setup(
+        cmdclass = {'bdist_wheel': bdist_wheel},
+        package_data = {'': [libname]},
+    )
+
+
+if __name__ == '__main__':
+    libname = sourcebuild_main()
+    build(lambda: lib_setup(libname), SourceBuild)
