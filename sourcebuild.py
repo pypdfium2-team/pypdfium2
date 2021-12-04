4b0ab7e320d7bc843309d48faf34305bf40f86e8
mara004 <geisserml@gmail.com>
2021-12-04 16:32:18 +0100

    Slightly shorten PATH mods

--- sourcebuild.py
+++ sourcebuild.py
@@ -131,7 +131,7 @@ def pack(src_libpath):
 
 def main():
     
-    os.environ['PATH'] = os.environ['PATH'] + f":{DepotToolsDir}"
+    os.environ['PATH'] += f":{DepotToolsDir}"
     
     dl_depottools()
     dl_pdfium()
