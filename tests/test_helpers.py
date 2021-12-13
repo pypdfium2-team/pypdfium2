cc76c043c7408d82bf0666a55ef300ebc1eeac78
mara004 <geisserml@gmail.com>
2021-12-13 18:46:20 +0100

    add BSD-3-Clause to some missing files

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2021 geisserml <geisserml@gmail.com>
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
 import io
 import pytest
