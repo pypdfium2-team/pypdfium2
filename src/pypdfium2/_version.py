05a139b5c935382a4e6cab115178975120e945ed
mara004 <geisserml@gmail.com>
2021-12-13 16:47:00 +0100

    Increment version

--- src/pypdfium2/_version.py
+++ src/pypdfium2/_version.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 V_MAJOR = 0
-V_MINOR = 3
+V_MINOR = 4
 V_PATCH = 0
 V_BETA  = None
 
@@ -10,4 +10,4 @@ V_PYPDFIUM2 = "{}.{}.{}".format(V_MAJOR, V_MINOR, V_PATCH)
 if V_BETA is not None:
     V_PYPDFIUM2 += "b{}".format(V_BETA)
 
-V_LIBPDFIUM = 4752
+V_LIBPDFIUM = 4763
