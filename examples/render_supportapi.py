45c0f50585bf56de8ecf2bec650b332cc4b2e219
geisserml <geisserml@gmail.com>
2021-12-14 14:34:21 +0100

    changes to examples

--- examples/render_supportapi.py
+++ examples/render_supportapi.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: 2021 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: CC-BY-4.0 
 
+import os
 import sys
 import pypdfium2 as pdfium
 
@@ -14,14 +15,21 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     
     with pdfium.PdfContext(filename) as pdf:
-        pil_image = pdfium.render_page(
-            pdf,
-            page_index = 0,
+        
+        n_pages = pdfium.FPDF_GetPageCount(pdf)
+        page_indices = [i for i in range(n_pages)]
+        
+        generator = pdfium.render_pdf(
+            filename,
+            page_indices = page_indices,
             scale = 1,
             rotation = 0,
             background_colour = 0xFFFFFFFF,
             render_annotations = True,
+            greyscale = False,
             optimise_mode = pdfium.OptimiseMode.none,
+            n_processes = os.cpu_count(),
         )
-    
-    pil_image.save("out.png")
+        
+        for image, suffix in generator:
+            image.save(f"out_{suffix}.png")
