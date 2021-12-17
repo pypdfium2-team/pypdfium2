657df21c714a591da21e6f0cc04ed610a6cab1f1
mara004 <geisserml@gmail.com>
2021-12-17 14:26:46 +0100

    examples/supportapi: correct indentation
    As `render_pdf()` takes the file name as input rather than the PDFium
    document, the close call can happen a lot earlier.

--- examples/render_supportapi.py
+++ examples/render_supportapi.py
@@ -15,22 +15,22 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     
     with pdfium.PdfContext(filename) as pdf:
-        
         n_pages = pdfium.FPDF_GetPageCount(pdf)
-        page_indices = [i for i in range(n_pages)]
-        
-        generator = pdfium.render_pdf(
-            filename,
-            page_indices = page_indices,
-            scale = 1,
-            rotation = 0,
-            colour = 0xFFFFFFFF,
-            annotations = True,
-            greyscale = False,
-            optimise_mode = pdfium.OptimiseMode.none,
-            n_processes = os.cpu_count(),
-        )
-        
-        for image, suffix in generator:
-            image.save(f"out_{suffix}.png")
-            image.close()
+    
+    page_indices = [i for i in range(n_pages)]
+    
+    generator = pdfium.render_pdf(
+        filename,
+        page_indices = page_indices,
+        scale = 1,
+        rotation = 0,
+        colour = 0xFFFFFFFF,
+        annotations = True,
+        greyscale = False,
+        optimise_mode = pdfium.OptimiseMode.none,
+        n_processes = os.cpu_count(),
+    )
+    
+    for image, suffix in generator:
+        image.save(f"out_{suffix}.png")
+        image.close()
