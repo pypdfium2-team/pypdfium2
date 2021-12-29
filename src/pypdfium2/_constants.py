6ed97db3153d681d3f5d7d6411fd2e0c13399c70
mara004 <geisserml@gmail.com>
2021-12-29 19:32:31 +0100

    constants/`ViewMode`: add reference to pdf manual

--- src/pypdfium2/_constants.py
+++ src/pypdfium2/_constants.py
@@ -6,13 +6,18 @@ from enum import Enum
 
 class OptimiseMode (Enum):
     """ How to optimise page rendering """
+    
     none = 0          #: Do not use any optimisations
     lcd_display = 1   #: Optimise for LCD displays
     printing = 2      #: Optimise for printing
 
 
 class ViewMode (Enum):
-    """ Modes that define how target coordinates of a bookmark should be interpreted. """
+    """
+    Modes that define how target coordinates of a bookmark should be interpreted.
+    Refer to the PDF 1.6 reference manual, section 8.2 for more information.
+    """
+    
     Unknown = 0
     XYZ     = 1
     Fit     = 2
