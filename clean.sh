cca4c004f011ac9947544cf6de11abc0179d8555
mara004 <geisserml@gmail.com>
2022-01-21 17:26:26 +0100

    clean: don't try to delete objects that should not exist

--- clean.sh
+++ clean.sh
@@ -3,13 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
 rm -r dist
-rm -r build
 rm -r data/*
 touch data/.gitkeep
 rm -r src/pypdfium2.egg-info/
 rm -r __pycache__
 rm -r src/pypdfium2/__pycache__
-unlink src/pypdfium2/_pypdfium.py
-unlink src/pypdfium2/pdfium
-unlink src/pypdfium2/pdfium.dll
-unlink src/pypdfium2/pdfium.dylib
