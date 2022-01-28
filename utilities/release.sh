40a8b1a9f1e2165e1beea30f5224040be24f92eb
mara004 <geisserml@gmail.com>
2022-01-28 14:50:40 +0100

    utilities/release: add checks for unused imports

--- utilities/release.sh
+++ utilities/release.sh
@@ -2,6 +2,11 @@
 # SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
+# check for unused imports
+importchecker src/
+importchecker tests/
+for pyfile in *.py; do importchecker "$pyfile"; done
+
 bash utilities/clean.sh
 python3 update_pdfium.py
 bash utilities/setup_all.sh
