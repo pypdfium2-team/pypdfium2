80c8c34ef3903a2100a94202b67c576aa86c45c4
mara004 <geisserml@gmail.com>
2022-01-30 10:56:36 +0100

    entrypoint test: minor improvements

--- tests/test_cli_entrypoint.py
+++ tests/test_cli_entrypoint.py
@@ -19,6 +19,8 @@ def test_entrypoint():
     
     namespace = {}
     exec("from {} import {}".format(module_path, method_name), namespace)
+    
+    assert method_name in namespace
     function = namespace[method_name]
     
     assert callable(function)
