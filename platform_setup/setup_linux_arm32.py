dbb2f64fbb4207571cdca974938844b4cd900d6b
geisserml <geisserml@gmail.com>
2022-02-10 15:45:35 +0100

    improve some more imports

--- platform_setup/setup_linux_arm32.py
+++ platform_setup/setup_linux_arm32.py
@@ -2,6 +2,6 @@
 # SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
-from setup_base import PlatformDirs, wheel_for
+from platform_setup.setup_base import PlatformDirs, wheel_for
 
 wheel_for(PlatformDirs.LinuxArm32)
