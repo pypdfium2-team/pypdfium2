645f91fb294f598650a2a8d6c63f652c517ade36
geisserml <geisserml@gmail.com>
2022-02-11 14:34:23 +0100

    packaging_base: improve comment

--- platform_setup/packaging_base.py
+++ platform_setup/packaging_base.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause 
 
-# Non-stdlib imports not allowed in this file, as it is imported in setup.py prior to the getdeps call
+# Non-stdlib imports not allowed in this file, as it is imported prior to the getdeps call
 
 import subprocess
 from os.path import (
