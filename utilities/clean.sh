9ee9494c1f256dc3a079311dc1c7f9b30796860f
geisserml <geisserml@gmail.com>
2022-02-12 12:38:49 +0100

    utilities/clean: skip deleting pycaches

--- utilities/clean.sh
+++ utilities/clean.sh
@@ -3,8 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
 rm -r src/pypdfium2.egg-info/
-rm -r __pycache__
-rm -r src/pypdfium2/__pycache__
 rm -r dist
 rm -r data/*
 echo "InitialState" > data/setup_status.txt
