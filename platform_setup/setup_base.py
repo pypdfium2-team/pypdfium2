3f62187655fa755453ce7dd0b2389d237ba44e73
mara004 <geisserml@gmail.com>
2022-02-16 14:39:11 +0100

    Add means to use multiple platform tags (#66)

--- platform_setup/setup_base.py
+++ platform_setup/setup_base.py
@@ -53,6 +53,18 @@ def _clean():
             os.remove(file)
 
 
+class CleanerContext:
+    
+    def __init__(self):
+        pass
+    
+    def __enter__(self):
+        _clean()
+    
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        _clean()
+
+
 def _copy_bindings(platform_dir):
     
     # non-recursively collect all objects from the platform directory
@@ -64,45 +76,83 @@ def _copy_bindings(platform_dir):
             shutil.copy(src_path, dest_path)
 
 
+def _get_mac_tag(arch):
+    return 'macosx_10_11_{}.macosx_11_0_{}'.format(arch, arch)
+
 def _get_linux_tag(arch):
-    # we would like to add `.manylinux2014_{}` as second tag, but `wheel` would automatically replace the dot, unfortunately (related discussion: https://github.com/pypa/wheel/issues/407)
-    return "manylinux_2_17_{}".format(arch)
+    return 'manylinux_2_17_{}.manylinux2014_{}'.format(arch, arch)
 
 
-def _get_tag(plat_dir):
+def _get_tags(plat_dir):
     if plat_dir is PlatformDirs.Darwin64:
-        return 'macosx_10_11_x86_64'
+        return _get_mac_tag('x86_64'), 'tmp_d64'
     elif plat_dir is PlatformDirs.DarwinArm64:
-        return 'macosx_11_0_arm64'
+        return _get_mac_tag('arm64'), 'tmp_darm64'
     elif plat_dir is PlatformDirs.Linux64:
-        return _get_linux_tag('x86_64')
+        return _get_linux_tag('x86_64'), 'tmp_l64'
     elif plat_dir is PlatformDirs.LinuxArm64:
-        return _get_linux_tag('aarch64')
+        return _get_linux_tag('aarch64'), 'tmp_larm64'
     elif plat_dir is PlatformDirs.LinuxArm32:
-        return _get_linux_tag('armv7l')
+        return _get_linux_tag('armv7l'), 'tmp_larm32'
     elif plat_dir is PlatformDirs.Windows64:
-        return 'win_amd64'
+        return 'win_amd64', None
     elif plat_dir is PlatformDirs.Windows86:
-        return 'win32'
+        return 'win32', None
     elif plat_dir is PlatformDirs.WindowsArm64:
-        return 'win_arm64'
+        return 'win_arm64', None
     elif plat_dir is PlatformDirs.SourceBuild:
-        return sysconfig.get_platform()
+        return sysconfig.get_platform(), None
     else:
         raise ValueError( "Unknown platform directory {}".format(plat_dir) )
 
-   
+
+def _rename_wheel(temp_tag, actual_tag):
+    
+    dist_dir = join(SourceTree, 'dist')
+    assert os.path.isdir(dist_dir)
+    
+    found_names = [f for f in os.listdir(dist_dir) if temp_tag in f]
+    assert len(found_names) == 1
+    
+    src_path = join(dist_dir, found_names[0])
+    assert os.path.isfile(src_path)
+    dest_path = src_path.replace(temp_tag, actual_tag, 1)
+    
+    print( "Renaming wheel: {} -> {}".format(src_path, dest_path) )
+    os.rename(src_path, dest_path)
+    
+
 SetupKws = dict(
     version = extract_version('V_PYPDFIUM2'),
 )
 
-def wheel_for(platform_dir):
-    _clean()
-    _copy_bindings(platform_dir)
-    tag = _get_tag(platform_dir)
-    setuptools.setup(
-        cmdclass = {'bdist_wheel': _get_bdist(tag)},
-        package_data = {'': Libnames},
-        **SetupKws,
+
+def mkwheel(platform_dir):
+    
+    actual_tag, temp_tag = _get_tags(platform_dir)
+    if temp_tag is None:
+        bdist_entry = _get_bdist(actual_tag)
+    else:
+        bdist_entry = _get_bdist(temp_tag)
+    
+    extra_kws = dict(
+        cmdclass = {'bdist_wheel': bdist_entry},
     )
-    _clean()
+    mkwheel_install(platform_dir, extra_kws)
+    
+    if temp_tag is not None:
+        _rename_wheel(temp_tag, actual_tag)
+
+
+def mkwheel_install(platform_dir, extra_kws=None):
+    
+    if extra_kws is None:
+        extra_kws = dict()
+    
+    with CleanerContext():
+        _copy_bindings(platform_dir)
+        setuptools.setup(
+            package_data = {'': Libnames},
+            **extra_kws,
+            **SetupKws,
+        )
