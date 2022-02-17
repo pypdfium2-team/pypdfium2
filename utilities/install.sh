d966dd57f2d31d1c64d7d1d1901eb77c9e7841fd
geisserml <geisserml@gmail.com>
2022-02-17 17:30:18 +0100

    install: avoid calling getdeps multiple times

--- utilities/install.sh
+++ utilities/install.sh
@@ -2,6 +2,5 @@
 # SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
-python3 platform_setup/check_deps.py
 python3 -m pip install . -v --no-build-isolation
 printf "InitialState" > platform_setup/setup_status.txt
