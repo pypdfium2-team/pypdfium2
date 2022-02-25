5d374bc83e324a4ecc6b75fd96ed547c76f861c3
geisserml <geisserml@gmail.com>
2022-02-25 19:27:25 +0100

    clean.sh: avoid errors if target does not exist

--- utilities/clean.sh
+++ utilities/clean.sh
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
-rm -r src/pypdfium2.egg-info/
-rm -r dist
-rm -r data/*
-rm tests/output/*
+rm -rf src/pypdfium2.egg-info/
+rm -rf dist
+rm -rf data/*
+rm -f tests/output/*
