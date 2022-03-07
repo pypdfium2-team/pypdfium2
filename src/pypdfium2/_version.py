d756431d9d6257ce679bbd8856d963396a7d73fa
geisserml <geisserml@gmail.com>
2022-03-07 17:56:01 +0100

    Prepare version for next release

--- src/pypdfium2/_version.py
+++ src/pypdfium2/_version.py
@@ -1,13 +1,13 @@
 # SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
-V_MAJOR = 0
-V_MINOR = 15
+V_MAJOR = 1
+V_MINOR = 0
 V_PATCH = 0
-V_BETA = None
+V_BETA = 1
 
 V_PYPDFIUM2 = "{}.{}.{}".format(V_MAJOR, V_MINOR, V_PATCH)  #: pypdfium2 version string
 if V_BETA is not None:
     V_PYPDFIUM2 += "b{}".format(V_BETA)
 
-V_LIBPDFIUM = 4915  #: PDFium library version integer (git tag)
+V_LIBPDFIUM = 4929  #: PDFium library version integer (git tag)
