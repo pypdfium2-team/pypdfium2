70497b41aac81c59913835647bf52e324bab2dcb
geisserml <geisserml@gmail.com>
2022-04-17 23:23:57 +0200

    version.py: improve docstring integration

--- src/pypdfium2/_version.py
+++ src/pypdfium2/_version.py
@@ -6,8 +6,10 @@ V_MINOR = 4
 V_PATCH = 0
 V_BETA = None
 
-V_PYPDFIUM2 = "%s.%s.%s" % (V_MAJOR, V_MINOR, V_PATCH)  #: pypdfium2 version string
+#: pypdfium2 version string
+V_PYPDFIUM2 = "%s.%s.%s" % (V_MAJOR, V_MINOR, V_PATCH)
 if V_BETA is not None:
     V_PYPDFIUM2 += "b%s" % V_BETA
 
-V_LIBPDFIUM = 4997  #: PDFium library version integer (git tag)
+#: PDFium library version integer (git tag)
+V_LIBPDFIUM = 4997
