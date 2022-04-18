eed5e2deaa4d1ef0f43030789c874e760040790d
geisserml <geisserml@gmail.com>
2022-04-18 20:19:14 +0200

    make releasnotes script slighly nicer

--- utilities/mk_releasenotes.py
+++ utilities/mk_releasenotes.py
@@ -8,6 +8,7 @@ from os.path import dirname, abspath, join
 
 SourceTree = dirname(dirname(abspath(__file__)))
 Git = shutil.which('git')
+RepositoryURL = 'https://github.com/pypdfium2-team/pypdfium2'
 
 
 def run_command(command, **kwargs):
@@ -17,16 +18,14 @@ def run_command(command, **kwargs):
     return output
 
 
-def _generate_tags_list():
+def generate_tags_list():
     tags_str = run_command([Git, 'for-each-ref', '--sort=creatordate', '--format', '%(refname)', 'refs/tags'])
     tags_list = tags_str.split('\n')
     tags_list.reverse()
     return tags_list
 
-TagsList = _generate_tags_list()
 
-
-def get_tag(n_descends, skip_beta=False):
+def get_tag(TagsList, n_descends, skip_beta=False):
     
     i = 0
     for line in TagsList:
@@ -40,14 +39,19 @@ def get_tag(n_descends, skip_beta=False):
     return tag
 
 
+def get_url(tag):
+    return RepositoryURL + '/tree/%s' % tag
+
+
 def main():
     
-    current_tag, prev_tag = get_tag(0), get_tag(1, True)
+    TagsList = generate_tags_list()
+    current_tag, prev_tag = get_tag(TagsList, 0), get_tag(TagsList, 1, True)
     print(current_tag, prev_tag)
     
     relnotes = "Release %s\n\n" % current_tag
-    relnotes += "### Changes\n\nCommits between `%s` and `%s`:\n\n" % (prev_tag, current_tag)
-    relnotes += run_command([Git, 'log', '%s..%s' % (prev_tag, current_tag), "--pretty=format:* %h %s"])
+    relnotes += "### Changes\n\nCommits between [`%s`](%s) and [`%s`](%s):\n\n" % (prev_tag, get_url(prev_tag), current_tag, get_url(current_tag))
+    relnotes += run_command([Git, 'log', '%s..%s' % (prev_tag, current_tag), "--pretty=format:* %H %s"])
     relnotes += '\n'
     
     with open(join(SourceTree, 'RELEASE.md'), 'w') as fh:
