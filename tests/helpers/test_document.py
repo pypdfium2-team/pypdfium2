df71513f57ba5eb0b9b851d93584dad2b12a883f
geisserml <geisserml@gmail.com>
2022-05-10 19:16:55 +0200

    move some test cases from `test_document` to `test_toc`

--- tests/helpers/test_document.py
+++ tests/helpers/test_document.py
@@ -7,9 +7,7 @@ from ..conftest import (
     TestFiles,
     iterate_testfiles,
 )
-from pytest import approx
 import pypdfium2 as pdfium
-from pypdfium2._cli.toc import print_toc
 
 
 def _pdfdoc_load(input_obj):
@@ -53,61 +51,3 @@ def test_pdfdoc_save():
     doc.close()
     assert buffer.tell() > 100000
     buffer.close()
-
-
-def _compare_bookmark(bookmark, title, page_index, view_mode, view_pos):
-    assert bookmark.title == title
-    assert bookmark.page_index == page_index
-    assert bookmark.view_mode == view_mode
-    approx(bookmark.view_pos) == view_pos
-
-
-def test_pdfdoc_gettoc():
-    
-    doc = pdfium.PdfDocument(TestFiles.bookmarks)
-    toc = doc.get_toc()
-    
-    # check first bookmark
-    _compare_bookmark(
-        next(toc),
-        title = "One",
-        page_index = 0,
-        view_mode = pdfium.ViewMode.XYZ,
-        view_pos = (89, 758, 0),
-    )
-    
-    # check common values
-    for bookmark in toc:
-        assert isinstance(bookmark, pdfium.OutlineItem)
-        assert bookmark.view_mode is pdfium.ViewMode.XYZ
-        assert round(bookmark.view_pos[0]) == 89
-    
-    # check last bookmark
-    _compare_bookmark(
-        bookmark,
-        title = "Three-B",
-        page_index = 1,
-        view_mode = pdfium.ViewMode.XYZ,
-        view_pos = (89, 657, 0),
-    )
-    
-    doc.close()
-
-
-def test_pdfdoc_gettoc_maxdepth():
-    
-    doc = pdfium.PdfDocument(TestFiles.bookmarks)
-    toc = doc.get_toc(max_depth=1)
-    
-    i = 0
-    for bookmark in toc:
-        i+= 1
-    assert i == 3
-    
-    doc.close()
-
-
-def test_pdfdoc_gettoc_print():
-    doc = pdfium.PdfDocument(TestFiles.bookmarks)
-    print_toc( doc.get_toc(max_depth=1) )
-    doc.close()
--- tests/helpers/test_toc.py
+++ tests/helpers/test_toc.py
@@ -2,21 +2,71 @@
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
 import logging
+from pytest import approx
 import pypdfium2 as pdfium
 from pypdfium2._cli.toc import print_toc
 from ..conftest import TestFiles
 
 
-def test_read_toc():
+def _compare_bookmark(bookmark, title, page_index, view_mode, view_pos):
+    assert bookmark.title == title
+    assert bookmark.page_index == page_index
+    assert bookmark.view_mode == view_mode
+    approx(bookmark.view_pos) == view_pos
+
+
+def test_gettoc():
+    
+    doc = pdfium.PdfDocument(TestFiles.bookmarks)
+    toc = doc.get_toc()
+    
+    # check first bookmark
+    _compare_bookmark(
+        next(toc),
+        title = "One",
+        page_index = 0,
+        view_mode = pdfium.ViewMode.XYZ,
+        view_pos = (89, 758, 0),
+    )
+    
+    # check common values
+    for bookmark in toc:
+        assert isinstance(bookmark, pdfium.OutlineItem)
+        assert bookmark.view_mode is pdfium.ViewMode.XYZ
+        assert round(bookmark.view_pos[0]) == 89
+    
+    # check last bookmark
+    _compare_bookmark(
+        bookmark,
+        title = "Three-B",
+        page_index = 1,
+        view_mode = pdfium.ViewMode.XYZ,
+        view_pos = (89, 657, 0),
+    )
+    
+    doc.close()
+
+
+def test_gettoc_circular(caplog):
+    with caplog.at_level(logging.CRITICAL):
+        with pdfium.PdfContext(TestFiles.bookmarks_circular) as pdf:
+            toc = pdfium.get_toc(pdf)
+            print_toc(toc)
+            assert "circular bookmark reference" in caplog.text
+
+
+def test_printtoc():
     with pdfium.PdfContext(TestFiles.bookmarks) as pdf:
         toc = pdfium.get_toc(pdf)
         print()
         print_toc(toc)
 
 
-def test_read_toc_circular(caplog):
-    with caplog.at_level(logging.CRITICAL):
-        with pdfium.PdfContext(TestFiles.bookmarks_circular) as pdf:
-            toc = pdfium.get_toc(pdf)
-            print_toc(toc)
-            assert "circular bookmark reference" in caplog.text
+def test_gettoc_maxdepth():
+    
+    doc = pdfium.PdfDocument(TestFiles.bookmarks)
+    toc = [b for b in doc.get_toc(max_depth=1)]
+    assert len(toc) == 3
+    print_toc(toc)
+    
+    doc.close()
