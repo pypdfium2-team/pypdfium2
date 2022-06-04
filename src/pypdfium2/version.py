b9ee1e9ebb088fd35534ee7ccc1882a32b02e389
geisserml <geisserml@gmail.com>
2022-06-04 19:24:33 +0200

    version: update docstrings

--- src/pypdfium2/version.py
+++ src/pypdfium2/version.py
@@ -6,12 +6,12 @@ V_MINOR = 0
 V_PATCH = 0
 V_BETA = None
 
-#: pypdfium2 version string
+#: pypdfium2 version string.
 V_PYPDFIUM2 = "%s.%s.%s" % (V_MAJOR, V_MINOR, V_PATCH)
 if V_BETA is not None:
     V_PYPDFIUM2 += "b%s" % V_BETA
 
-#: PDFium library version string (git tag or commit hash)
+#: PDFium library version string (git tag or commit hash).
 V_LIBPDFIUM = "5092"
 
 #: Whether the included PDFium binary was built from source locally (:data:`True`) or downloaded from pdfium-binaries (:data:`False`).
