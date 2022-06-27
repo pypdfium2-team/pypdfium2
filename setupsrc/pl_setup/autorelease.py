ff5bf6b00d7d659d49c65ad2843912eb9e25dde3
geisserml <geisserml@gmail.com>
2022-06-27 12:42:51 +0200

    Add script to further automate the release process

---
+++ setupsrc/pl_setup/autorelease.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python3
+# SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
+import sys
+import time
+import shutil
+from os.path import (
+    join,
+    abspath,
+    dirname,
+)
+
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+import pl_setup.packaging_base as pkg_base
+import pl_setup.update_pdfium as fpdf_up
+
+
+def run_cmd(*args, **kws):
+    return pkg_base.run_cmd(*args, **kws, cwd=pkg_base.SourceTree)
+
+
+def update_version():
+    
+    v_before = pkg_base.VerNamespace["V_MINOR"]
+    latest = fpdf_up.get_latest_version()
+    fpdf_up.handle_versions(latest)
+    
+    v_after = pkg_base.VerNamespace["V_MINOR"]
+    if v_before == v_after:
+        pkg_base.set_version("V_PATCH", 1)
+        pkg_base.set_version("V_BETA", None)
+    
+    return (pkg_base.VerNamespace, pkg_base.get_version_ns())
+
+
+def update_changelog(prev_ns, curr_ns):
+    
+    Changelog = join(pkg_base.SourceTree, "docs", "source", "changelog.md")
+    
+    message = 3*"\n" + "## %s (%s)\n - " % (
+        curr_ns["V_PYPDFIUM2"],
+        time.strftime("%Y-%m-%d"),
+    )
+    if prev_ns["V_LIBPDFIUM"] != curr_ns["V_LIBPDFIUM"]:
+        message += "Updated PDFium from `%s` to `%s`" % (prev_ns["V_LIBPDFIUM"], curr_ns["V_LIBPDFIUM"])
+    else:
+        message += "No PDFium update"
+    message += " (autorelease)."
+    
+    with open(Changelog, "r") as fh:
+        content = fh.read()
+        exp = "# Changelog"
+        pos = content.index(exp) + len(exp)
+        content = content[:pos] + message + content[pos:]
+    with open(Changelog, "w") as fh:
+        fh.write(content)
+
+
+def set_tag(curr_ns):
+    Git = shutil.which("git")
+    run_cmd([Git, "tag", "-a", curr_ns["V_PYPDFIUM2"], "-m", "Autorelease"])
+    # TODO push tag
+    run_cmd([Git, "checkout", "stable"])
+    run_cmd([Git, "rebase", "main"])
+    # TODO push update to stable branch
+    run_cmd([Git, "checkout", "main"])
+
+
+def main():
+    prev_ns, curr_ns = update_version()
+    update_changelog(prev_ns, curr_ns)
+    set_tag(curr_ns)
+
+
+if __name__ == "__main__":
+    main()
