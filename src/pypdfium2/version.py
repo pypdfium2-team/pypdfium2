03b379f99ac0a45f9eb62273303f450c70b13734
geisserml <geisserml@gmail.com>
2022-09-24 16:26:23 +0000

    [autorelease] update changelog and version file

--- src/pypdfium2/version.py
+++ src/pypdfium2/version.py
@@ -3,10 +3,10 @@
 
 __all__ = ["V_PYPDFIUM2", "V_LIBPDFIUM", "IS_SOURCEBUILD"]
 
-V_MAJOR = 2
-V_MINOR = 11
+V_MAJOR = 3
+V_MINOR = 0
 V_PATCH = 0
-V_BETA = None
+V_BETA = 1
 
 #: pypdfium2 version string.
 V_PYPDFIUM2 = "%s.%s.%s" % (V_MAJOR, V_MINOR, V_PATCH)
@@ -14,7 +14,7 @@ if V_BETA is not None:
     V_PYPDFIUM2 += "b%s" % V_BETA
 
 #: PDFium library version string (git tag or commit hash).
-V_LIBPDFIUM = "5254"
+V_LIBPDFIUM = "5316"
 
 #: Whether the included PDFium binary was built from source locally (:data:`True`) or downloaded from pdfium-binaries (:data:`False`).
 IS_SOURCEBUILD = False
