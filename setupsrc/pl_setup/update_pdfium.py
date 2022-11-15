4da5e7c43a4a27486bf3e13e1c3d20597e77c6ca
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-15 08:23:16 +0000

    Adding tarfile member sanitization to extractall()

--- setupsrc/pl_setup/update_pdfium.py
+++ setupsrc/pl_setup/update_pdfium.py
@@ -74,7 +74,26 @@ def unpack_archives(archives):
     for pl_name, file_path in archives.items():
         extraction_path = join(DataTree, pl_name, "build_tar")
         with tarfile.open(file_path) as archive:
-            archive.extractall(extraction_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, extraction_path)
         os.remove(file_path)
 
 
