f004c63cd78e2a0454e81c558b1a0fe57175f159
geisserml <geisserml@gmail.com>
2023-07-07 23:39:14 +0200

    cli/render: fix & enhance rendering with custom color scheme

--- src/pypdfium2/_cli/render.py
+++ src/pypdfium2/_cli/render.py
@@ -11,10 +11,13 @@ from pypdfium2._cli._parsers import add_input, get_input
 logger = logging.getLogger(__name__)
 
 
-ColorOpts = dict(
-    metavar = "C",
-    nargs = 4,
-    type = int,
+CsFields = ("path_fill", "path_stroke", "text_fill", "text_stroke")
+ColorOpts = dict(metavar="C", nargs=4, type=int)
+DefaultDarkTheme = dict(
+    path_fill   = (255, 255, 255, 255),
+    path_stroke = (255, 255, 255, 255),
+    text_fill   = (255, 255, 255, 255),
+    text_stroke = (255, 255, 255, 255),
 )
 
 
@@ -50,7 +53,6 @@ def attach(parser):
     )
     parser.add_argument(
         "--fill-color",
-        default = (255, 255, 255, 255),
         help = "Color the bitmap will be filled with before rendering. It shall be given in RGBA format as a sequence of integers ranging from 0 to 255. Defaults to white.",
         **ColorOpts,
     )
@@ -120,6 +122,11 @@ def attach(parser):
         description = "Options for rendering with custom color scheme",
     )
     color_scheme.add_argument(
+        "--default-dark",
+        action = "store_true",
+        help = "Whether to use a dark scheme as default. If given, the parameters below selectively override defaults."
+    )
+    color_scheme.add_argument(
         "--path-fill",
         **ColorOpts
     )
@@ -156,21 +163,19 @@ def main(args):
     
     if not args.prefix:
         args.prefix = f"{args.input.stem}_"
+    if not args.fill_color:
+        args.fill_color = (0, 0, 0, 255) if args.default_dark else (255, 255, 255, 255)
     
-    cs_kwargs = dict(
-        path_fill = args.path_fill,
-        path_stroke = args.path_stroke,
-        text_fill = args.text_fill,
-        text_stroke = args.text_stroke,
-    )
+    cs_kwargs = dict()
+    if args.default_dark:
+        cs_kwargs.update(**DefaultDarkTheme)
+    cs_kwargs.update(**{f: getattr(args, f) for f in CsFields if getattr(args, f)})
+    
     cs = None
-    if all(cs_kwargs.values()):
-        cs = pdfium.PdfColorScheme(
-            fill_to_stroke = args.fill_to_stroke,
-            **cs_kwargs,
-        )
-    elif any(cs_kwargs.values()):
-        raise ValueError("If rendering with custom color scheme, all parameters need to be set explicitly.")
+    if len(cs_kwargs) > 0:
+        if len(cs_kwargs) < len(CsFields):
+            logger.warning("All color scheme params should be set explicitly. Unset elements may be missing/transparent.")
+        cs = pdfium.PdfColorScheme(**cs_kwargs)
     
     kwargs = dict(
         page_indices = args.pages,
@@ -180,6 +185,7 @@ def main(args):
         grayscale = args.grayscale,
         fill_color = args.fill_color,
         color_scheme = cs,
+        fill_to_stroke = args.fill_to_stroke,
         optimize_mode = args.optimize_mode,
         draw_annots = not args.no_annotations,
         may_draw_forms = not args.no_forms,
