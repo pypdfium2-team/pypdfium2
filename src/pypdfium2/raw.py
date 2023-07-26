c7f27a207b8c7798d48e5749b72d1f43c53b6335
geisserml <geisserml@gmail.com>
2023-07-26 15:47:04 +0200

    nits

--- src/pypdfium2/raw.py
+++ src/pypdfium2/raw.py
@@ -2,15 +2,22 @@
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
 import threading
+import functools
 
 PdfiumMutex = threading.Lock()
+
+def _make_threadsafe(f):
+    @functools.wraps(f)
+    def pdfium_callable_threadsafe(*args, **kwargs):
+        with PdfiumMutex:
+            return f(*args, **kwargs)
+    return pdfium_callable_threadsafe
 
 
 def _get_members():
     
     import ctypes
     import fnmatch
-    import functools
     from types import ModuleType
     import pypdfium2._raw_unsafe
     
@@ -18,16 +25,9 @@ def _get_members():
         (str.startswith, ("_", "struct_", "enum_")),
         (str.endswith, ("_t__", )),
         (str.__contains__, ("LibraryLoader", )),
-        (fnmatch.fnmatchcase, ("uint*_t", "fpdf_*_t"))
-        (str.__eq__, ("load_library", "UNCHECKED", "c_ptrdiff_t")),
+        (fnmatch.fnmatchcase, ("fpdf_*_t", )),
+        (str.__eq__, ("load_library", "add_library_search_dirs", "UNCHECKED", "c_ptrdiff_t")),
     )
-    
-    def make_threadsafe(f):
-        @functools.wraps(f)
-        def pdfium_callable_threadsafe(*args, **kwargs):
-            with PdfiumMutex:
-                return f(*args, **kwargs)
-        return pdfium_callable_threadsafe
     
     for name in dir(pypdfium2._raw_unsafe):
         
@@ -41,10 +41,12 @@ def _get_members():
         
         if isinstance(member, ctypes._CFuncPtr):
             # make pdfium functions thread safe
-            globals()[name] = make_threadsafe(member)
+            globals()[name] = _make_threadsafe(member)
         else:
             globals()[name] = member
+    
+    fnmatch._compile_pattern.cache_clear()
 
 
 _get_members()
-del threading, _get_members
+del threading, functools, _make_threadsafe, _get_members
