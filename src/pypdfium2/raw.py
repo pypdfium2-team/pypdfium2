5a8f2a4a34ba85abf05a25c9f34dc0efee397d86
geisserml <geisserml@gmail.com>
2023-07-27 15:09:32 +0200

    continue

--- src/pypdfium2/raw.py
+++ src/pypdfium2/raw.py
@@ -1,6 +1,11 @@
 # SPDX-FileCopyrightText: 2023 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
+# This code has 2 main purposes:
+# a) Wrap pdfium functions with a mutex so they can safely be used in a threaded context.
+# b) Try to mask the garbage exposed by ctypesgen (as far as possible), to achieve a purer namespace.
+#    This is a bit controversial since it may negatively impact import performance, and normally python devs say "practicality beats purity" ...
+
 import threading
 import functools
 
@@ -16,36 +21,31 @@ def _make_threadsafe(f):
 
 def _get_members():
     
+    import re
     import ctypes
     import fnmatch
     from types import ModuleType
     import pypdfium2._raw_unsafe
     
+    _compile_wildcards = lambda *patterns: [re.compile(fnmatch.translate(p)) for p in patterns]
+    
     NAME_FILTERS = (
-        (str.startswith, ("_", "struct_", "enum_")),
-        (str.endswith, ("_t__", )),
-        (str.__contains__, ("LibraryLoader", )),
-        (fnmatch.fnmatchcase, ("fpdf_*_t", )),
-        (str.__eq__, ("load_library", "add_library_search_dirs", "UNCHECKED", "c_ptrdiff_t")),
+        (str.startswith, ["_", "struct_", "enum_"]),
+        (str.endswith, ["_t__"]),
+        (str.__contains__, ["LibraryLoader"]),
+        (str.__eq__, ["load_library", "add_library_search_dirs", "UNCHECKED", "c_ptrdiff_t"]),
+        (lambda n, x: x.match(n), _compile_wildcards("fpdf_*_t")),
     )
     
     for name in dir(pypdfium2._raw_unsafe):
-        
         member = getattr(pypdfium2._raw_unsafe, name)
-        
-        # try to mask the garbage exposed by ctypesgen, as far as possible
         if any(any(func(name, x) for x in skips) for func, skips in NAME_FILTERS):
             continue
         if isinstance(member, ModuleType) or hasattr(ctypes, name):
             continue
-        
         if isinstance(member, ctypes._CFuncPtr):
-            # make pdfium functions thread safe
-            globals()[name] = _make_threadsafe(member)
-        else:
-            globals()[name] = member
-    
-    fnmatch._compile_pattern.cache_clear()
+            member = _make_threadsafe(member)
+        globals()[name] = member
 
 
 _get_members()
