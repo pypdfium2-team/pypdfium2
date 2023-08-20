98a5f02aeedd0339719cd638d02030cf0e29e856
geisserml <geisserml@gmail.com>
2023-08-20 13:35:55 +0200

    Continue

--- src/pypdfium2/version.py
+++ src/pypdfium2/version.py
@@ -1,6 +1,9 @@
 # SPDX-FileCopyrightText: 2023 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
+# TODO consider switching this to a yaml file and only assemble V_PYPDFIUM2 in the library?
+# advantages: denotes this is a static data file, easier r/w, accessibility in conda recipe via load_data_file()
+
 __all__ = ("V_PYPDFIUM2", "V_LIBPDFIUM", "V_BUILDNAME", "V_PDFIUM_IS_V8")
 
 V_MAJOR = 4
@@ -19,6 +22,6 @@ V_LIBPDFIUM = "5868"
 #: String describing the included PDFium binary's origin (pdfium-binaries, source)
 V_BUILDNAME = "pdfium-binaries"
 
-# CONSIDER change to V_BUILDTYPE: str ?
+# TODO consider changing to V_IS_V8XFA: bool or V_BUILDTYPE: str ?
 #: Whether the included PDFium binary was built with V8 support or not
 V_PDFIUM_IS_V8 = False
