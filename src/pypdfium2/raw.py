1fa40b8f79ab407cd2f8ee962f6ff12d6c4cbe81
geisserml <geisserml@gmail.com>
2023-10-23 19:35:49 +0200

    nit: fix another comment

--- src/pypdfium2/raw.py
+++ src/pypdfium2/raw.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2023 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
-# platform files are managed in a separate pypdfium2_raw module, cleanly separating raw bindings and helpers
+# platform files are managed in an own pypdfium2_raw module, cleanly separating raw bindings and helpers
 from pypdfium2_raw.bindings import *
