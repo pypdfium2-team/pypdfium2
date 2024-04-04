0183d80a4cdc03356dae34c17ab8834d3c227d0c
geisserml <geisserml@gmail.com>
2024-04-04 17:21:44 +0200

    toc: update API test

--- tests/test_toc.py
+++ tests/test_toc.py
@@ -8,10 +8,19 @@ import pypdfium2.raw as pdfium_c
 from .conftest import TestResources
 
 
-def _compare_bookmark(bookmark, view_pos, **kwargs):
-    for name, exp_value in kwargs.items():
-        assert exp_value == getattr(bookmark, name)
-    assert pytest.approx(bookmark.view_pos, abs=1) == view_pos
+def _compare_bookmark(bm, **kwargs):
+    assert isinstance(bm, pdfium.PdfBookmark)
+    assert kwargs["title"] == bm.get_title()
+    assert kwargs["count"] == bm.get_count()
+    dest = bm.get_dest()
+    if dest is None:
+        assert kwargs["dest"] is None
+    else:
+        assert isinstance(dest, pdfium.PdfDest)
+        assert kwargs["page_index"] == dest.get_index()
+        view_mode, view_pos = dest.get_view()
+        assert kwargs["view_mode"] == view_mode
+        assert kwargs["view_pos"] == pytest.approx(view_pos, abs=1)
 
 
 def test_gettoc():
@@ -26,25 +35,24 @@ def test_gettoc():
         page_index = 0,
         view_mode = pdfium_c.PDFDEST_VIEW_XYZ,
         view_pos = (89, 758, 0),
-        is_closed = True,
-        n_kids = 2,
+        count = -2,
     )
     
     # check common values
-    for bookmark in toc:
-        assert isinstance(bookmark, pdfium.PdfOutlineItem)
-        assert bookmark.view_mode is pdfium_c.PDFDEST_VIEW_XYZ
-        assert round(bookmark.view_pos[0]) == 89
+    for bm in toc:
+        dest = bm.get_dest()
+        view_mode, view_pos = dest.get_view()
+        assert view_mode is pdfium_c.PDFDEST_VIEW_XYZ
+        assert round(view_pos[0]) == 89
     
     # check last bookmark
     _compare_bookmark(
-        bookmark,
+        bm,
         title = "Three-B",
         page_index = 1,
         view_mode = pdfium_c.PDFDEST_VIEW_XYZ,
         view_pos = (89, 657, 0),
-        is_closed = None,
-        n_kids = 0,
+        count = 0,
     )
 
 
@@ -56,20 +64,14 @@ def test_gettoc_circular(caplog):
     _compare_bookmark(
         next(toc),
         title = "A Good Beginning",
-        page_index = None,
-        view_mode = pdfium_c.PDFDEST_VIEW_UNKNOWN_MODE,
-        view_pos = [],
-        is_closed = None,
-        n_kids = 0,
+        dest = None,
+        count = 0,
     )
     _compare_bookmark(
         next(toc),
         title = "A Good Ending",
-        page_index = None,
-        view_mode = pdfium_c.PDFDEST_VIEW_UNKNOWN_MODE,
-        view_pos = [],
-        is_closed = None,
-        n_kids = 0,
+        dest = None,
+        count = 0,
     )
     with caplog.at_level(logging.WARNING):
         for other in toc: pass
