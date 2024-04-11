1fcbfd81667df834522ede507e8099c9b1932aaa
geisserml <geisserml@gmail.com>
2024-04-11 19:06:20 +0200

    fix `__all__` blunder
    ("...") reduces to "...", which is also iterable (but quite wrong),
    leading to `AttributeError: module 'pypdfium2._helpers.page' has no
    attribute 'P'`
    
    Perhaps it would be smarter to use lists rather than tuples anyway.

--- src/pypdfium2/_helpers/page.py
+++ src/pypdfium2/_helpers/page.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2024 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
-__all__ = ("PdfPage")
+__all__ = ("PdfPage", )
 
 import math
 import ctypes
