caee9ea24a6223951248b834d389b95ce26278e5
geisserml <geisserml@gmail.com>
2025-04-03 20:54:28 +0200

    Slightly update comment in _library_scope.py

--- src/pypdfium2/_library_scope.py
+++ src/pypdfium2/_library_scope.py
@@ -14,7 +14,7 @@ def init_lib():
         print("Initialize PDFium", file=sys.stderr)
     
     # PDFium init API may change in the future: https://crbug.com/pdfium/1446
-    # NOTE Technically, FPDF_InitLibrary() would be sufficient for our purposes, but since it's formally marked for deprecation, don't use it to be on the safe side. Also, avoid experimental config versions that might not be promoted to stable.
+    # NOTE Technically, FPDF_InitLibrary() would be sufficient for our purposes, but pdfium docs say "This will be deprecated in the future", so don't use it to be on the safe side. Also, avoid experimental config versions that might not be promoted to stable.
     config = pdfium_c.FPDF_LIBRARY_CONFIG(
         version = 2,
         m_pUserFontPaths = None,
@@ -24,7 +24,6 @@ def init_lib():
         # m_RendererType = pdfium_c.FPDF_RENDERERTYPE_AGG,  # v4
     )
     pdfium_c.FPDF_InitLibraryWithConfig(config)
-    
     pdfium_i.LIBRARY_AVAILABLE.value = True
 
 
