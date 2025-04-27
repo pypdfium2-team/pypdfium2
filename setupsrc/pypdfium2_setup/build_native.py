c6f8248f060cef587c26ce1d2a98c55d6f4a7513
geisserml <geisserml@gmail.com>
2025-04-27 16:23:36 +0200

    build_native: update a comment

--- setupsrc/pypdfium2_setup/build_native.py
+++ setupsrc/pypdfium2_setup/build_native.py
@@ -284,7 +284,7 @@ def main_api(build_ver=None, with_tests=False, n_jobs=None, compiler=None, clang
     
     n_commits, hash = 0, None
     if build_ver == "main":
-        # we're not aware how to find out the post-tag info without an actual git repository, so set some placeholders indicating that this is a build from main
+        # we're not aware how to find out the post-tag info with a --depth 1 checkout, so set some placeholders indicating that this is a build from main
         n_commits, hash = float("inf"), ""
     
     pack_sourcebuild(PDFIUM_DIR, build_dir, full_ver, n_commits=n_commits, hash=hash)
