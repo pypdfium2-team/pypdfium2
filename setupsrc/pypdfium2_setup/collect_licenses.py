9425f04e4c66a73dad4e2bb0a94e94995b89a7d0
geisserml <geisserml@gmail.com>
2025-05-13 03:14:52 +0200

    collect_licenses: switch to argparse

--- setupsrc/pypdfium2_setup/collect_licenses.py
+++ setupsrc/pypdfium2_setup/collect_licenses.py
@@ -3,6 +3,7 @@
 
 import sys
 import shutil
+import argparse
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parents[1]))
@@ -13,10 +14,15 @@ from pypdfium2_setup.base import (
 
 def main():
     
-    first_arg = sys.argv[1] if len(sys.argv) > 1 else None
-    if first_arg in ("-h", "--help"):
-        print(f"Run {sys.argv[1]} without arguments to list licenses, or with 'copy' to collect licenses into //BUILD_LICENSES. Note: This assumes a prior run of update.py")
-    do_collect = first_arg == "copy"
+    parser = argparse.ArgumentParser(
+        description = "List/collect build licenses, assuming a prior run of update.py",
+    )
+    parser.add_argument(
+        "--copy",
+        action = "store_true",
+        help = "Collect licenses into //BUILD_LICENSES directory",
+    )
+    args = parser.parse_args()
     
     data_dirs = [p for p in DataDir.iterdir() if p.is_dir()]
     data_dirs.remove(DataDir_Bindings)
@@ -34,7 +40,7 @@ def main():
         print(f"    {platforms}")
         print()
     
-    if do_collect:
+    if args.copy:
         dest_dir = ProjectDir/"BUILD_LICENSES"
         if dest_dir.exists():
             shutil.rmtree(dest_dir)
