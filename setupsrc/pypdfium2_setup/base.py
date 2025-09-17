5d7443619ec3b2758b9c8c4499aa2495d52e1727
geisserml <geisserml@gmail.com>
2025-09-17 12:57:13 +0200

    Register post_pdfium pin

--- setupsrc/pypdfium2_setup/base.py
+++ setupsrc/pypdfium2_setup/base.py
@@ -270,7 +270,8 @@ class _PdfiumVerClass:
     
     @cached_property
     def pinned(self):
-        # comments are not permitted in JSON, so the reason for the post_pdfium pin (if set) goes here: (currently no post_pdfium pin)
+        # comments are not permitted in JSON, so the reason for the post_pdfium pin (if set) goes here:
+        # verification requires pdfium-binaries >= 7415
         record = read_json(AR_RecordFile)
         return record["post_pdfium"] or record["pdfium"]
 
