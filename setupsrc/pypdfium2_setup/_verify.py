683c8cec7fb7348b65c1d196fdf9471f656239b8
geisserml <geisserml@gmail.com>
2025-10-04 15:51:43 +0200

    verify: use authenticated verification if available

--- setupsrc/pypdfium2_setup/_verify.py
+++ setupsrc/pypdfium2_setup/_verify.py
@@ -33,6 +33,9 @@ def get_gh_avail():
         log("gh CLI version is too old for verification")
         return False
 
+def _get_gh_auth_state():
+    return run_cmd(["gh", "auth", "status"], cwd=None, check=False).returncode == 0
+
 def _gh_web_api(path):
     log(f"API Request {path}")
     with url_request.urlopen("https://api.github.com"+path) as h:
@@ -56,34 +59,43 @@ def _get_sha256sum(path):
         
         return hash.hexdigest()
 
+
 def do_verify(archives, pdfium_version, have_recent_gh, auto_enabled):
     
     # https://github.com/cli/cli/issues/11803#issuecomment-3334820737
     
     if not have_recent_gh:
-        raise SystemExit(f"--verify requires gh CLI >= {MIN_GH_FOR_VERIFY} (authenticated)")
+        raise SystemExit(f"--verify requires gh CLI >= {MIN_GH_FOR_VERIFY}")
     if pdfium_version < MIN_PDFIUM_FOR_VERIFY:
         raise SystemExit(f"--verify was passed, but the requested pdfium version is too low: {MIN_PDFIUM_FOR_VERIFY}")
     
-    attest_path = DataDir/f"pdfium-binaries-{pdfium_version}-attestation.json"
     artifact_paths = tuple(archives.values())
     one_artifact = artifact_paths[0]
-    if not attest_path.exists():
-        file_sum = _get_sha256sum(one_artifact)
-        try:
-            attest_json = _gh_web_api(f"/repos/bblanchon/pdfium-binaries/attestations/sha256:{file_sum}")
-        except HTTPError as e:
-            if auto_enabled and "rate limit exceeded" in str(e):
-                log("Warning: Unable to auto-verify due to rate limit.")
-                return
-            else:
-                raise e
-        attest_json = attest_json["attestations"][0]["bundle"]
-        with attest_path.open("w") as fh:
-            json.dump(attest_json, fh)
+    
+    is_authed = _get_gh_auth_state()
+    if is_authed:
+        log("gh is authenticated, using default verification")
+        extra_args = []
+    else:
+        log("gh is not authenticated, attempting tokenless verification (may fail due to rate limit)")
+        attest_path = DataDir/f"pdfium-binaries-{pdfium_version}-attestation.json"
+        if not attest_path.exists():
+            file_sum = _get_sha256sum(one_artifact)
+            try:
+                attest_json = _gh_web_api(f"/repos/bblanchon/pdfium-binaries/attestations/sha256:{file_sum}")
+            except HTTPError as e:
+                if auto_enabled and "rate limit exceeded" in str(e):
+                    log("Warning: Unable to auto-verify due to rate limit.")
+                    return
+                else:
+                    raise e
+            attest_json = attest_json["attestations"][0]["bundle"]
+            with attest_path.open("w") as fh:
+                json.dump(attest_json, fh)
+        extra_args = ["-b", str(attest_path)]
     
     verify_result = run_cmd(
-        ["gh", "attestation", "verify", "-R", "bblanchon/pdfium-binaries", str(one_artifact), "-b", str(attest_path), "--format=json"],
+        ["gh", "attestation", "verify", "-R", "bblanchon/pdfium-binaries", str(one_artifact), *extra_args, "--format=json"],
         cwd=DataDir, check=True, capture=True
     )
     log(f"{one_artifact.name}: verified by gh")
