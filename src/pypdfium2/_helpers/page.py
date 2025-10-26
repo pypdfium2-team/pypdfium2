pypdfium2-team/pypdfium2

commit 56650c449b37238d06f051ad438ad3c3c7382bc8

Licenses branch (#357)

src/pypdfium2/_helpers/page.py
@@ -240,9 +240,17 @@ class PdfPage (pdfium_i.AutoCloseable):
         if pageobj.page is not self:
             raise ValueError("The pageobject you attempted to remove is not part of this page.")
         
-        ok = pdfium_c.FPDFPage_RemoveObject(self, pageobj)
+        if pageobj.level > 0:
+            assert pageobj.container is not None
+            ok = pdfium_c.FPDFFormObj_RemoveObject(pageobj.container, pageobj)
+            pageobj.level, pageobj.container = 0, None
+        else:
+            assert pageobj.container is None
+            ok = pdfium_c.FPDFPage_RemoveObject(self, pageobj)
+        
         if not ok:
             raise PdfiumError("Failed to remove pageobject.")
+        
         pageobj.page = None
         pageobj._attach_finalizer()
     
@@ -277,7 +285,7 @@ class PdfPage (pdfium_i.AutoCloseable):
         if form:
             count_objects = pdfium_c.FPDFFormObj_CountObjects
             get_object = pdfium_c.FPDFFormObj_GetObject
-            parent = form
+            parent = form.raw
         else:
             count_objects = pdfium_c.FPDFPage_CountObjects
             get_object = pdfium_c.FPDFPage_GetObject
@@ -294,7 +302,7 @@ class PdfPage (pdfium_i.AutoCloseable):
                 raise PdfiumError("Failed to get pageobject.")
             
             # Don't register as child object, because the lifetime of pageobjects that are part of a page is managed by pdfium. The parent page should remain alive while a pageobject is used, but it seems unjustified to store countless of weakrefs just to lock pageobjects when the parent page is closed.
-            helper_obj = PdfObject(raw_obj, page=self, pdf=self.pdf, level=level)
+            helper_obj = PdfObject(raw_obj, page=self, pdf=self.pdf, container=form, level=level)
             if not filter or helper_obj.type in filter:
                 yield helper_obj
             
@@ -302,7 +310,7 @@ class PdfPage (pdfium_i.AutoCloseable):
                 yield from self.get_objects(
                     filter = filter,
                     max_depth = max_depth,
-                    form = raw_obj,
+                    form = helper_obj,
                     level = level + 1,
                 )
     

.github/dependabot.yml
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2024 GitHub
 # SPDX-FileCopyrightText: 2025 geisserml <geisserml@gmail.com>
-# SPDX-License-Identifier: LicenseRef-FairUse OR CC-BY-4.0
+# SPDX-License-Identifier: CC-BY-4.0
 
 # Set update schedule for GitHub Actions
 

.github/workflows/test_sourcebuild.yaml
@@ -18,8 +18,8 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        # NOTE On GH actions, macOS <=13 is Intel, whereas macOS >=14 will be ARM64
-        # NOTE Google's toolchain doesn't seem to run on Linux arm64 natively. The toolchain-free build should work, though.
+        # On GH actions, macOS <=13 is Intel, whereas macOS >=14 will be ARM64
+        # Google's toolchain doesn't seem to run on Linux arm64 natively. The toolchain-free build (or cross-compilation from x86_64) should work, though.
         os: ['ubuntu-latest', 'macos-13', 'macos-14', 'windows-latest']  # 'ubuntu-24.04-arm'
         build_mode: ['toolchained']
         include:
@@ -81,7 +81,6 @@ jobs:
       - name: Build PDFium (native, clang)
         if: ${{ matrix.build_mode == 'native-clang' }}
         run: |
-          # NOTE alternatively, --clang-path /usr/lib/llvm-18 might work on Ubuntu? Not sure.
           VERSION=18
           ARCH=$(uname -m)
           sudo apt-get install -y llvm lld
@@ -90,7 +89,7 @@ jobs:
           python3 ./setupsrc/pypdfium2_setup/build_native.py --compiler clang --test
       
       - name: Install
-        run: PDFIUM_PLATFORM="sourcebuild" python3 -m pip install .
+        run: PDFIUM_PLATFORM="sourcebuild" python3 -m pip install -v .
       
       # must show matching pdfium version with sourcebuild origin
       - name: Check version

BUILD_LICENSES/libjpeg_turbo.ijg
@@ -1,0 +1,260 @@
+libjpeg-turbo note:  This file has been modified by The libjpeg-turbo Project
+to include only information relevant to libjpeg-turbo, to wordsmith certain
+sections, and to remove impolitic language that existed in the libjpeg v8
+README.  It is included only for reference.  Please see README.md for
+information specific to libjpeg-turbo.
+
+
+The Independent JPEG Group's JPEG software
+==========================================
+
+This distribution contains a release of the Independent JPEG Group's free JPEG
+software.  You are welcome to redistribute this software and to use it for any
+purpose, subject to the conditions under LEGAL ISSUES, below.
+
+This software is the work of Tom Lane, Guido Vollbeding, Philip Gladstone,
+Bill Allombert, Jim Boucher, Lee Crocker, Bob Friesenhahn, Ben Jackson,
+Julian Minguillon, Luis Ortiz, George Phillips, Davide Rossi, Ge' Weijers,
+and other members of the Independent JPEG Group.
+
+IJG is not affiliated with the ISO/IEC JTC1/SC29/WG1 standards committee
+(also known as JPEG, together with ITU-T SG16).
+
+
+DOCUMENTATION ROADMAP
+=====================
+
+This file contains the following sections:
+
+OVERVIEW            General description of JPEG and the IJG software.
+LEGAL ISSUES        Copyright, lack of warranty, terms of distribution.
+REFERENCES          Where to learn more about JPEG.
+ARCHIVE LOCATIONS   Where to find newer versions of this software.
+FILE FORMAT WARS    Software *not* to get.
+TO DO               Plans for future IJG releases.
+
+Other documentation files in the distribution are:
+
+User documentation:
+  doc/usage.txt         Usage instructions for cjpeg, djpeg, jpegtran,
+                        rdjpgcom, and wrjpgcom.
+  doc/*.1               Unix-style man pages for programs (same info as
+                        usage.txt).
+  doc/wizard.txt        Advanced usage instructions for JPEG wizards only.
+  doc/change.log        Version-to-version change highlights.
+Programmer and internal documentation:
+  doc/libjpeg.txt       How to use the JPEG library in your own programs.
+  src/example.c         Sample code for calling the JPEG library.
+  doc/structure.txt     Overview of the JPEG library's internal structure.
+  doc/coderules.txt     Coding style rules --- please read if you contribute
+                        code.
+
+Please read at least usage.txt.  Some information can also be found in the JPEG
+FAQ (Frequently Asked Questions) article.  See ARCHIVE LOCATIONS below to find
+out where to obtain the FAQ article.
+
+If you want to understand how the JPEG code works, we suggest reading one or
+more of the REFERENCES, then looking at the documentation files (in roughly
+the order listed) before diving into the code.
+
+
+OVERVIEW
+========
+
+This package contains C software to implement JPEG image encoding, decoding,
+and transcoding.  JPEG (pronounced "jay-peg") is a standardized compression
+method for full-color and grayscale images.  JPEG's strong suit is compressing
+photographic images or other types of images that have smooth color and
+brightness transitions between neighboring pixels.  Images with sharp lines or
+other abrupt features may not compress well with JPEG, and a higher JPEG
+quality may have to be used to avoid visible compression artifacts with such
+images.
+
+JPEG is normally lossy, meaning that the output pixels are not necessarily
+identical to the input pixels.  However, on photographic content and other
+"smooth" images, very good compression ratios can be obtained with no visible
+compression artifacts, and extremely high compression ratios are possible if
+you are willing to sacrifice image quality (by reducing the "quality" setting
+in the compressor.)
+
+This software implements JPEG baseline, extended-sequential, progressive, and
+lossless compression processes.  Provision is made for supporting all variants
+of these processes, although some uncommon parameter settings aren't
+implemented yet.  We have made no provision for supporting the hierarchical
+processes defined in the standard.
+
+We provide a set of library routines for reading and writing JPEG image files,
+plus two sample applications "cjpeg" and "djpeg", which use the library to
+perform conversion between JPEG and some other popular image file formats.
+The library is intended to be reused in other applications.
+
+In order to support file conversion and viewing software, we have included
+considerable functionality beyond the bare JPEG coding/decoding capability;
+for example, the color quantization modules are not strictly part of JPEG
+decoding, but they are essential for output to colormapped file formats.  These
+extra functions can be compiled out of the library if not required for a
+particular application.
+
+We have also included "jpegtran", a utility for lossless transcoding between
+different JPEG processes, and "rdjpgcom" and "wrjpgcom", two simple
+applications for inserting and extracting textual comments in JFIF files.
+
+The emphasis in designing this software has been on achieving portability and
+flexibility, while also making it fast enough to be useful.  In particular,
+the software is not intended to be read as a tutorial on JPEG.  (See the
+REFERENCES section for introductory material.)  Rather, it is intended to
+be reliable, portable, industrial-strength code.  We do not claim to have
+achieved that goal in every aspect of the software, but we strive for it.
+
+We welcome the use of this software as a component of commercial products.
+No royalty is required, but we do ask for an acknowledgement in product
+documentation, as described under LEGAL ISSUES.
+
+
+LEGAL ISSUES
+============
+
+In plain English:
+
+1. We don't promise that this software works.  (But if you find any bugs,
+   please let us know!)
+2. You can use this software for whatever you want.  You don't have to pay us.
+3. You may not pretend that you wrote this software.  If you use it in a
+   program, you must acknowledge somewhere in your documentation that
+   you've used the IJG code.
+
+In legalese:
+
+The authors make NO WARRANTY or representation, either express or implied,
+with respect to this software, its quality, accuracy, merchantability, or
+fitness for a particular purpose.  This software is provided "AS IS", and you,
+its user, assume the entire risk as to its quality and accuracy.
+
+This software is copyright (C) 1991-2020, Thomas G. Lane, Guido Vollbeding.
+All Rights Reserved except as specified below.
+
+Permission is hereby granted to use, copy, modify, and distribute this
+software (or portions thereof) for any purpose, without fee, subject to these
+conditions:
+(1) If any part of the source code for this software is distributed, then this
+README file must be included, with this copyright and no-warranty notice
+unaltered; and any additions, deletions, or changes to the original files
+must be clearly indicated in accompanying documentation.
+(2) If only executable code is distributed, then the accompanying
+documentation must state that "this software is based in part on the work of
+the Independent JPEG Group".
+(3) Permission for use of this software is granted only if the user accepts
+full responsibility for any undesirable consequences; the authors accept
+NO LIABILITY for damages of any kind.
+
+These conditions apply to any software derived from or based on the IJG code,
+not just to the unmodified library.  If you use our work, you ought to
+acknowledge us.
+
+Permission is NOT granted for the use of any IJG author's name or company name
+in advertising or publicity relating to this software or products derived from
+it.  This software may be referred to only as "the Independent JPEG Group's
+software".
+
+We specifically permit and encourage the use of this software as the basis of
+commercial products, provided that all warranty or liability claims are
+assumed by the product vendor.
+
+
+REFERENCES
+==========
+
+We recommend reading one or more of these references before trying to
+understand the innards of the JPEG software.
+
+The best short technical introduction to the JPEG compression algorithm is
+        Wallace, Gregory K.  "The JPEG Still Picture Compression Standard",
+        Communications of the ACM, April 1991 (vol. 34 no. 4), pp. 30-44.
+(Adjacent articles in that issue discuss MPEG motion picture compression,
+applications of JPEG, and related topics.)  If you don't have the CACM issue
+handy, a PDF file containing a revised version of Wallace's article is
+available at http://www.ijg.org/files/Wallace.JPEG.pdf.  The file (actually
+a preprint for an article that appeared in IEEE Trans. Consumer Electronics)
+omits the sample images that appeared in CACM, but it includes corrections
+and some added material.  Note: the Wallace article is copyright ACM and IEEE,
+and it may not be used for commercial purposes.
+
+A somewhat less technical, more leisurely introduction to JPEG can be found in
+"The Data Compression Book" by Mark Nelson and Jean-loup Gailly, published by
+M&T Books (New York), 2nd ed. 1996, ISBN 1-55851-434-1.  This book provides
+good explanations and example C code for a multitude of compression methods
+including JPEG.  It is an excellent source if you are comfortable reading C
+code but don't know much about data compression in general.  The book's JPEG
+sample code is far from industrial-strength, but when you are ready to look
+at a full implementation, you've got one here...
+
+The best currently available description of JPEG is the textbook "JPEG Still
+Image Data Compression Standard" by William B. Pennebaker and Joan L.
+Mitchell, published by Van Nostrand Reinhold, 1993, ISBN 0-442-01272-1.
+Price US$59.95, 638 pp.  The book includes the complete text of the ISO JPEG
+standards (DIS 10918-1 and draft DIS 10918-2).
+
+The original JPEG standard is divided into two parts, Part 1 being the actual
+specification, while Part 2 covers compliance testing methods.  Part 1 is
+titled "Digital Compression and Coding of Continuous-tone Still Images,
+Part 1: Requirements and guidelines" and has document numbers ISO/IEC IS
+10918-1, ITU-T T.81.  Part 2 is titled "Digital Compression and Coding of
+Continuous-tone Still Images, Part 2: Compliance testing" and has document
+numbers ISO/IEC IS 10918-2, ITU-T T.83.
+
+The JPEG standard does not specify all details of an interchangeable file
+format.  For the omitted details, we follow the "JFIF" conventions, revision
+1.02.  JFIF version 1 has been adopted as ISO/IEC 10918-5 (05/2013) and
+Recommendation ITU-T T.871 (05/2011): Information technology - Digital
+compression and coding of continuous-tone still images: JPEG File Interchange
+Format (JFIF).  It is available as a free download in PDF file format from
+https://www.iso.org/standard/54989.html and http://www.itu.int/rec/T-REC-T.871.
+A PDF file of the older JFIF 1.02 specification is available at
+http://www.w3.org/Graphics/JPEG/jfif3.pdf.
+
+The TIFF 6.0 file format specification can be obtained from
+http://mirrors.ctan.org/graphics/tiff/TIFF6.ps.gz.  The JPEG incorporation
+scheme found in the TIFF 6.0 spec of 3-June-92 has a number of serious
+problems.  IJG does not recommend use of the TIFF 6.0 design (TIFF Compression
+tag 6).  Instead, we recommend the JPEG design proposed by TIFF Technical Note
+#2 (Compression tag 7).  Copies of this Note can be obtained from
+http://www.ijg.org/files/.  It is expected that the next revision
+of the TIFF spec will replace the 6.0 JPEG design with the Note's design.
+Although IJG's own code does not support TIFF/JPEG, the free libtiff library
+uses our library to implement TIFF/JPEG per the Note.
+
+
+ARCHIVE LOCATIONS
+=================
+
+The "official" archive site for this software is www.ijg.org.
+The most recent released version can always be found there in
+directory "files".
+
+The JPEG FAQ (Frequently Asked Questions) article is a source of some
+general information about JPEG.  It is available at
+http://www.faqs.org/faqs/jpeg-faq.
+
+
+FILE FORMAT COMPATIBILITY
+=========================
+
+This software implements ITU T.81 | ISO/IEC 10918 with some extensions from
+ITU T.871 | ISO/IEC 10918-5 (JPEG File Interchange Format-- see REFERENCES).
+Informally, the term "JPEG image" or "JPEG file" most often refers to JFIF or
+a subset thereof, but there are other formats containing the name "JPEG" that
+are incompatible with the original JPEG standard or with JFIF (for instance,
+JPEG 2000 and JPEG XR).  This software therefore does not support these
+formats.  Indeed, one of the original reasons for developing this free software
+was to help force convergence on a common, interoperable format standard for
+JPEG files.
+
+JFIF is a minimal or "low end" representation.  TIFF/JPEG (TIFF revision 6.0 as
+modified by TIFF Technical Note #2) can be used for "high end" applications
+that need to record a lot of additional data about an image.
+
+
+TO DO
+=====
+
+Please send bug reports, offers of help, etc. to jpeg-info@jpegclub.org.

MANIFEST.in
@@ -20,6 +20,7 @@ include src/pypdfium2_raw/__init__.py
 
 # Include all of licenses in an sdist (also wheel licenses), since we may build a wheel from the sdist
 recursive-include LICENSES/ *
+recursive-include BUILD_LICENSES/ *
 include REUSE.toml
 include REUSE-wheel.toml
 

REUSE-wheel.toml
@@ -1,7 +1,7 @@
 # This file contains copyright info for data files included in our wheel packages.
 # See also REUSE.toml regarding copyright of data files in the project directory.
-
 # Note, this file has been auto-converted from .reuse/dep5-wheel by `reuse convert-dep5`
+# TODO(geisserml) convert to informal markdown
 
 version = 1
 SPDX-PackageName = "pypdfium2"
@@ -34,10 +34,10 @@ path = [
 precedence = "aggregate"
 SPDX-FileCopyrightText = [
     "2025 PDFium developers",
-    "2025 Developers of projects mentioned in PdfiumThirdParty",
+    "2025 Developers of PDFium's dependencies (see BUILD_LICENSES/)",
     "2025 Benoît Blanchon and pdfium-binaries contributors"
 ]
-SPDX-License-Identifier = "BSD-3-Clause AND Apache-2.0 AND LicenseRef-PdfiumThirdParty"
+SPDX-License-Identifier = "BSD-3-Clause AND Apache-2.0 AND BUILD_LICENSES"
 SPDX-FileComment = '''
 pdfium's LICENSE file includes both BSD-3-Clause and Apache-2.0 texts
 We're not sure if this meant as SPDX "AND" or "OR", so use the conservative (safe) assumption "AND".

REUSE.toml
@@ -11,11 +11,11 @@ SPDX-PackageDownloadLocation = "https://github.com/pypdfium2-team/pypdfium2"
 
 [[annotations]]
 path = [
-    "req/**.txt",
+    "req/*.txt",
     "autorelease/record.json",
     "autorelease/config.json",
     ".github/PULL_REQUEST_TEMPLATE.md",
-    "tests/expectations/**",
+    "tests/expectations/*.txt",
     "tests/resources/render.pdf",
     "tests/resources/multipage.pdf",
     "tests/resources/encrypted.pdf",
@@ -40,11 +40,11 @@ SPDX-FileComment = "Auto-generated by ctypesgen. Probably not copyrighted."
 path = "tests/resources/toc.pdf"
 precedence = "aggregate"
 SPDX-FileCopyrightText = "2020 Matthias Erll"
-SPDX-License-Identifier = "LicenseRef-FairUse"
+SPDX-License-Identifier = "MPL-2.0"
 SPDX-FileComment = '''
-Obtained from: https://github.com/pikepdf/pikepdf/blob/master/tests/resources/outlines.pdf
-No individual license stated for this data file. Project license is MPL-2.0.
-(https://github.com/pikepdf/pikepdf/blob/2662d9da649a3b354422673fb62bcfe0a1b9ba4c/REUSE.toml#L154 says "License assumed from LICENSE.txt in project root.")
+Obtained from https://github.com/pikepdf/pikepdf/blob/master/tests/resources/outlines.pdf
+pikepdf's REUSE.toml says "License assumed from LICENSE.txt in project root.":
+https://github.com/pikepdf/pikepdf/blob/2662d9da649a3b354422673fb62bcfe0a1b9ba4c/REUSE.toml#L154
 '''
 
 [[annotations]]
@@ -55,20 +55,23 @@ path = [
     "tests/resources/forms.pdf",
     "tests/resources/attachments.pdf",
     "tests/resources/mona_lisa.jpg",
+    "tests/resources/form_object_with_image.pdf",
 ]
 precedence = "aggregate"
 SPDX-FileCopyrightText = "2022 PDFium developers"
 SPDX-License-Identifier = "BSD-3-Clause AND Apache-2.0"
 SPDX-FileComment = '''
-pdfium's LICENSE file includes both BSD-3-Clause and Apache-2.0 texts
-We're not sure if this meant as SPDX "AND" or "OR", so use the conservative (safe) assumption "AND".
-Obtained from:
+Test files obtained from pdfium's //testing/resources or the pdfium_tests corpus:
 https://pdfium.googlesource.com/pdfium/+/refs/heads/main/testing/resources/bookmarks_circular.pdf
 https://pdfium.googlesource.com/pdfium_tests/+/refs/heads/main/fx/other/8.2_outline.pdf
 https://pdfium.googlesource.com/pdfium_tests/+/refs/heads/main/fx/FRC_8.2.2_part1/FRC_51_8.2.2_T_8.4__Count_edit_count_100.pdf
 https://pdfium.googlesource.com/pdfium/+/refs/heads/main/testing/resources/listbox_form.pdf
 https://pdfium.googlesource.com/pdfium/+/refs/heads/main/testing/resources/embedded_attachments.pdf
 https://pdfium.googlesource.com/pdfium/+/refs/heads/main/testing/resources/mona_lisa.jpg
+https://pdfium.googlesource.com/pdfium/+/refs/heads/main/testing/resources/form_object_with_image.pdf
+
+pdfium's LICENSE file includes both BSD-3-Clause and Apache-2.0 texts
+We're not sure if this meant as SPDX "AND" or "OR", so use the conservative (safe) assumption "AND".
 '''
 
 [[annotations]]
@@ -91,12 +94,14 @@ SPDX-FileCopyrightText = [
     "2022 Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>",
     "2025 geisserml <geisserml@gmail.com>",
 ]
-SPDX-License-Identifier = "LicenseRef-FairUse"
+SPDX-License-Identifier = "LGPL-3.0-or-later"
+SPDX-FileComment = '''
+Obtained from img2pdf. No individual license stated for this data file.
+'''
 
 [[annotations]]
 path = [
     "pdfium_patches/public_headers.patch",
-    "pdfium_patches/single_lib.patch",
     "pdfium_patches/win/use_resources_rc.patch",
     "pdfium_patches/win/build.patch",
     "pdfium_patches/win/resources.rc",
@@ -107,19 +112,20 @@ SPDX-FileCopyrightText = [
     "2025 geisserml",
     "2025 PDFium developers",
 ]
-SPDX-License-Identifier = "LicenseRef-FairUse"
+SPDX-License-Identifier = "MIT"
 SPDX-FileComment = '''
 Derived from https://github.com/bblanchon/pdfium-binaries/tree/master/patches
-For reuse, see https://github.com/bblanchon/pdfium-binaries/issues/55
+See also //BUILD_LICENSES/pdfium-binaries.txt for a copy of upstream's license file as embedded in the tarballs.
 Unchanged/deleted code is by PDFium, added code by pdfium-binaries.
 '''
 
 [[annotations]]
 path = [
-    "pdfium_patches/siso.patch",
+    "pdfium_patches/single_lib.patch",
+    "pdfium_patches/ffp_contract.patch",
     "pdfium_patches/avoid_new_clang_flags.patch",
     "pdfium_patches/system_libcxx_with_clang.patch",
-    "pdfium_patches/ffp_contract.patch",
+    "pdfium_patches/siso.patch",
 ]
 precedence = "aggregate"
 SPDX-FileCopyrightText = [
@@ -129,18 +135,20 @@ SPDX-FileCopyrightText = [
 SPDX-License-Identifier = "BSD-3-Clause OR Apache-2.0"
 
 [[annotations]]
-path = "REUSE-wheel.toml"
-precedence = "aggregate"
-SPDX-FileCopyrightText = "2025 geisserml"
-SPDX-License-Identifier = "LicenseRef-PdfiumThirdParty"
-SPDX-FileComment = '''
-Pointer to prevent reuse lint from reporting LicenseRef-PdfiumThirdParty as unused.
-This file is not actually copyrighted, but it mentions the data files that are.
-'''
-
-[[annotations]]
-path = ["RELEASE.md", "docs/build/.gitkeep", "sbuild/.gitkeep"]
+path = [
+    "REUSE-wheel.toml",
+    "docs/build/.gitkeep",
+    "sbuild/.gitkeep",
+    "RELEASE.md",  # TODO add to gitignore?
+]
 precedence = "aggregate"
 SPDX-FileCopyrightText = "2025 geisserml"
 SPDX-License-Identifier = "CC-BY-4.0"
-SPDX-FileComment = "Other files. Listed here to make reuse lint happy on CI."
+SPDX-FileComment = "Other files"
+
+[[annotations]]
+path = ["BUILD_LICENSES/**"]
+precedence = "aggregate"
+SPDX-FileCopyrightText = "unknown"
+SPDX-License-Identifier = "LicenseRef-Ignore"
+SPDX-FileComment = "Licenses for compiled binaries of pdfium."

docs/source/conf.py
@@ -13,13 +13,15 @@ from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parents[2] / "setupsrc"))
 from pypdfium2_setup.base import (
-    parse_git_tag,
+    get_helpers_info,
+    get_next_changelog,
 )
 
 # RTD modifies conf.py, so we have to ignore dirty state if on RTD
 is_rtd = os.environ.get("READTHEDOCS", "").lower() == "true"
-tag_info = parse_git_tag()
-have_changes = tag_info["n_commits"] > 0 or (tag_info["dirty"] and not is_rtd)
+tag_info = get_helpers_info()
+next_changelog = get_next_changelog()
+have_changes = tag_info["n_commits"] != 0 or (tag_info["dirty"] and not is_rtd) or next_changelog
 
 project = "pypdfium2"
 author = "pypdfium2-team"

commit 7beb515cac37e3ed3c0e72f5ca998b44cff02af3

Clean up weird inconsistency in PdfPage.get_objects() code
Introduced in
https://github.com/pypdfium2-team/pypdfium2/commit/56650c449b37238d06f051ad438ad3c3c7382bc8#diff-3d8268d1bacb872d3153db0a3ded75b35dc9b6c8db8432f628d09488494f5a02
Not sure why that happened.

src/pypdfium2/_helpers/page.py
@@ -285,7 +285,7 @@ class PdfPage (pdfium_i.AutoCloseable):
         if form:
             count_objects = pdfium_c.FPDFFormObj_CountObjects
             get_object = pdfium_c.FPDFFormObj_GetObject
-            parent = form.raw
+            parent = form
         else:
             count_objects = pdfium_c.FPDFPage_CountObjects
             get_object = pdfium_c.FPDFPage_GetObject