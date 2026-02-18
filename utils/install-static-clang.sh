a1de585226fbef07f6cd314a302eafeb769a066a
geisserml <geisserml@gmail.com>
2026-02-18 03:10:28 +0100

    Update static clang
    https://github.com/scikit-build/ninja-python-distributions/blob/master/scripts/install-static-clang.sh
    https://github.com/scikit-build/cmake-python-distributions/blob/main/scripts/install-static-clang.sh

--- utils/install-static-clang.sh
+++ utils/install-static-clang.sh
@@ -23,19 +23,19 @@ case "${STATIC_CLANG_ARCH}" in
 	s390x) GO_ARCH=s390x;;
 	*) echo "No static-clang toolchain for ${CLANG_ARCH}">2; exit 1;;
 esac
-STATIC_CLANG_VERSION=21.1.6.0
+STATIC_CLANG_VERSION=21.1.8.1
 STATIC_CLANG_FILENAME="static-clang-linux-${GO_ARCH}.tar.xz"
 STATIC_CLANG_URL="https://github.com/mayeut/static-clang-images/releases/download/v${STATIC_CLANG_VERSION}/${STATIC_CLANG_FILENAME}"
 pushd /tmp
 cat<<'EOF' | grep "${STATIC_CLANG_FILENAME}" > "${STATIC_CLANG_FILENAME}.sha256"
-3f92a131d27ca606dae8230550236a0c897a7f5990d61a293814e0abea8d0e1f  static-clang-linux-386.tar.xz
-3fc6a3500cb9514b2c3af6d4a95676842769c301f872b6cea8c15576a64e756c  static-clang-linux-amd64.tar.xz
-82ea0c148ec75f72a2f6f61cc877561efe9675c6e59a1a2c4d130f088f9dc868  static-clang-linux-arm.tar.xz
-9b5ad28877b6d56aff530164f7f88590e5d3441a1fddd7a73370539783056120  static-clang-linux-arm64.tar.xz
-2adccbcad99d033222c8a63872739919375a7aef2339ce2e8ab7dcfc938502b1  static-clang-linux-loong64.tar.xz
-5f551911ad73ecbbcf278e6d05a04bc68bd0dc4918a6a145352072f7734959c6  static-clang-linux-ppc64le.tar.xz
-90f5beda1004bec124607df1f9fc0a70c2b9f382b82ab1db2703ebd131c920ef  static-clang-linux-riscv64.tar.xz
-e4047765a5e64bace4be36f6aae4d859e96bc1298d3ff5ba6b7d6100ea7d23f7  static-clang-linux-s390x.tar.xz
+583980309e73fa753c0791e05deceac7cb06c5444956d61189f1d651941bcd8e  static-clang-linux-386.tar.xz
+f539f1fd24bcc07ecc220594022907865914236540091fbf189cb496a9a3751c  static-clang-linux-amd64.tar.xz
+d0c8b7fac4734cc8048fda3b1f33ccdf24d26f81a52df53348e5fa84ab4b203a  static-clang-linux-arm.tar.xz
+57fb9cf798b3a2c4b0f3c31c0b7fe3803b5319d2af075407e1aef0cd57882f65  static-clang-linux-arm64.tar.xz
+5a2b296e9030d0320d9e4eac8859e5db0504cf1d4169981af233cb339292fa4c  static-clang-linux-loong64.tar.xz
+c8541b2d36f0fd8f13ddabe1292b6fb7550a742e76e0f1f70fb29fcee073be4a  static-clang-linux-ppc64le.tar.xz
+b311137f955b55139b02e8c1d7ec259628404a0563d136df7a817a33784bd2bf  static-clang-linux-riscv64.tar.xz
+de74fd8e5de244d36398684b2afa67add2311df6ccfa24f3c08a1d777ca814fa  static-clang-linux-s390x.tar.xz
 EOF
 curl -fsSLO "${STATIC_CLANG_URL}"
 sha256sum -c "${STATIC_CLANG_FILENAME}.sha256"
