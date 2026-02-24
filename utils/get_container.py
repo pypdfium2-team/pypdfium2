8d120830a18b92d377564a559b342be1b8757e0d
geisserml <geisserml@gmail.com>
2026-02-24 19:28:56 +0100

    Try to fix alpine init shell

--- utils/get_container.py
+++ utils/get_container.py
@@ -3,7 +3,7 @@
 
 import sys
 
-_DEBIAN_CMD = "apt update; apt install --no-install-recommends -y python3 python3-pip python3-venv python3-pillow python3-numpy python3-pytest"
+_DEBIAN_CMD = "apt update && apt install --no-install-recommends -y python3 python3-pip python3-venv python3-pillow python3-numpy python3-pytest"
 _ALPINE_CMD = "apk add python3 py3-pip py3-pillow py3-numpy py3-pytest"
 
 DOCKER_CPU_MAP = {
@@ -19,13 +19,14 @@ def _get_container(os_class, cpu):
     if cpu == "loongarch64":
         prefix = f"ghcr.io/{prefix}"
     if os_class == "manylinux":
-        return f"{prefix}/debian:trixie-slim", _DEBIAN_CMD
+        return f"{prefix}/debian:trixie-slim", _DEBIAN_CMD, "bash"
     elif os_class == "musllinux":
-        return f"{prefix}/alpine:3", _ALPINE_CMD
+        return f"{prefix}/alpine:3", _ALPINE_CMD, "sh"
     else:
         assert False, os_class
 
 os_class, cpu = sys.argv[1].split("_", maxsplit=1)
-container, deps = _get_container(os_class, cpu)
-print(f'export CONTAINER={container!r}')
-print(f'export PREPARE_CMD={deps!r}')
+container, prepare_cmd, shell = _get_container(os_class, cpu)
+print(f'export CONTAINER="{container}"')
+print(f'export PREPARE_CMD="{prepare_cmd}"')
+print(f'export INIT_SHELL="{shell}"')
