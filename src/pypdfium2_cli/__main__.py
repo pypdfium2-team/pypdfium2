pypdfium2-team/pypdfium2

commit 47dffe8ca0b8ca57cd59611a89526fea398ed1a5

Try to fix macOS and python < 3.8

src/pypdfium2_cli/__main__.py
@@ -4,31 +4,34 @@
 import sys
 import argparse
 import importlib
-import functools
 from os.path import basename
+from pypdfium2._lazy import cached_property
 from pypdfium2_cli._setup import setup_logging
 
-
-@functools.cache
-def init():
-    global PYPDFIUM_INFO, PDFIUM_INFO, _libs
-    from pypdfium2.version import PYPDFIUM_INFO, PDFIUM_INFO
-    from pypdfium2_raw.bindings import _libs
+class _InitClass:
     
-    global SubCommands, CmdToModule
-    SubCommands = {
-        "arrange":        "Rearrange/merge documents",
-        "attachments":    "List/extract/edit embedded files",
-        "extract-images": "Extract images",
-        "extract-text":   "Extract text",
-        "imgtopdf":       "Convert images to PDF",
-        "pageobjects":    "Print info on pageobjects",
-        "pdfinfo":        "Print info on document and pages",
-        "render":         "Rasterize pages",
-        "tile":           "Tile pages (N-up)",
-        "toc":            "Print table of contents",
-    }
-    CmdToModule = {n: importlib.import_module(f"pypdfium2_cli.{n.replace('-', '_')}") for n in SubCommands}
+    @cached_property
+    def imports(self):
+        global PYPDFIUM_INFO, PDFIUM_INFO, _libs
+        from pypdfium2.version import PYPDFIUM_INFO, PDFIUM_INFO
+        from pypdfium2_raw.bindings import _libs
+        
+        global SubCommands, CmdToModule
+        SubCommands = {
+            "arrange":        "Rearrange/merge documents",
+            "attachments":    "List/extract/edit embedded files",
+            "extract-images": "Extract images",
+            "extract-text":   "Extract text",
+            "imgtopdf":       "Convert images to PDF",
+            "pageobjects":    "Print info on pageobjects",
+            "pdfinfo":        "Print info on document and pages",
+            "render":         "Rasterize pages",
+            "tile":           "Tile pages (N-up)",
+            "toc":            "Print table of contents",
+        }
+        CmdToModule = {n: importlib.import_module(f"pypdfium2_cli.{n.replace('-', '_')}") for n in SubCommands}
+
+init = _InitClass()
 
 
 def get_parser():
@@ -73,7 +76,7 @@ Environment variables:
 
 def api_main(raw_args=sys.argv[1:]):
     
-    init()
+    init.imports
     parser = get_parser()
     args = parser.parse_args(raw_args)
     

tests/test_opener.py
@@ -141,7 +141,7 @@ def test_open_nonascii():
         traceback.print_exc()
 
 
-@pytest.mark.skipif(sys.platform.startswith("win32"), reason="Specific to non-Windows OSes")
+@pytest.mark.skipif(not sys.platform.startswith("linux"), reason="Specific to Linux")
 def test_open_garbled_filename():
     
     garbled_name = "CONTRATO DE LOCA\udcc7\udcc3O.pdf"

commit a6705a37a901ff9a42011f76fe7b37c64d3c5a27

fix: don't import from pypdfium2 before logging setup
https://github.com/pypdfium2-team/pypdfium2/commit/47dffe8ca0b8ca57cd59611a89526fea398ed1a5
introduced this regression

src/pypdfium2_cli/__main__.py
@@ -5,33 +5,29 @@ import sys
 import argparse
 import importlib
 from os.path import basename
-from pypdfium2._lazy import cached_property
+from functools import lru_cache
 from pypdfium2_cli._setup import setup_logging
 
-class _InitClass:
+@lru_cache(maxsize=1)
+def init():
+    global PYPDFIUM_INFO, PDFIUM_INFO, _libs
+    from pypdfium2.version import PYPDFIUM_INFO, PDFIUM_INFO
+    from pypdfium2_raw.bindings import _libs
     
-    @cached_property
-    def imports(self):
-        global PYPDFIUM_INFO, PDFIUM_INFO, _libs
-        from pypdfium2.version import PYPDFIUM_INFO, PDFIUM_INFO
-        from pypdfium2_raw.bindings import _libs
-        
-        global SubCommands, CmdToModule
-        SubCommands = {
-            "arrange":        "Rearrange/merge documents",
-            "attachments":    "List/extract/edit embedded files",
-            "extract-images": "Extract images",
-            "extract-text":   "Extract text",
-            "imgtopdf":       "Convert images to PDF",
-            "pageobjects":    "Print info on pageobjects",
-            "pdfinfo":        "Print info on document and pages",
-            "render":         "Rasterize pages",
-            "tile":           "Tile pages (N-up)",
-            "toc":            "Print table of contents",
-        }
-        CmdToModule = {n: importlib.import_module(f"pypdfium2_cli.{n.replace('-', '_')}") for n in SubCommands}
-
-init = _InitClass()
+    global SubCommands, CmdToModule
+    SubCommands = {
+        "arrange":        "Rearrange/merge documents",
+        "attachments":    "List/extract/edit embedded files",
+        "extract-images": "Extract images",
+        "extract-text":   "Extract text",
+        "imgtopdf":       "Convert images to PDF",
+        "pageobjects":    "Print info on pageobjects",
+        "pdfinfo":        "Print info on document and pages",
+        "render":         "Rasterize pages",
+        "tile":           "Tile pages (N-up)",
+        "toc":            "Print table of contents",
+    }
+    CmdToModule = {n: importlib.import_module(f"pypdfium2_cli.{n.replace('-', '_')}") for n in SubCommands}
 
 
 def get_parser():
@@ -76,7 +72,7 @@ Environment variables:
 
 def api_main(raw_args=sys.argv[1:]):
     
-    init.imports
+    init()
     parser = get_parser()
     args = parser.parse_args(raw_args)
     