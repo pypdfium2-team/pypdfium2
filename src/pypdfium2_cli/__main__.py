60f9bef9e429879f0a291fb3f646f7ea85bf97fc
geisserml <geisserml@gmail.com>
2026-04-25 03:02:18 +0200

    CLI: implement task from previous commit

--- src/pypdfium2_cli/__main__.py
+++ src/pypdfium2_cli/__main__.py
@@ -5,34 +5,30 @@ import sys
 import argparse
 import importlib
 from os.path import basename
-from functools import lru_cache
+# Important: Do not import from the core `pypdfium2` module here, because it initializes the library, which must be deferred until after setup_logging(). Importing from the other modules (e.g. pypdfium2_cli itself) is fine.
 from pypdfium2_cli._setup import setup_logging
 
-@lru_cache(maxsize=1)
-def init():
-    global PYPDFIUM_INFO, PDFIUM_INFO, _libs
+SubCommands = {
+    "arrange":        "Rearrange/merge documents",
+    "attachments":    "List/extract/edit embedded files",
+    "extract-images": "Extract images",
+    "extract-text":   "Extract text",
+    "imgtopdf":       "Convert images to PDF",
+    "pageobjects":    "Print info on pageobjects",
+    "pdfinfo":        "Print info on document and pages",
+    "fonts":          "List a document's fonts",
+    "render":         "Rasterize pages",
+    "tile":           "Tile pages (N-up)",
+    "toc":            "Print table of contents",
+}
+
+def get_version():
     from pypdfium2.version import PYPDFIUM_INFO, PDFIUM_INFO
     from pypdfium2_raw.bindings import _libs
-    
-    global SubCommands, CmdToModule
-    SubCommands = {
-        "arrange":        "Rearrange/merge documents",
-        "attachments":    "List/extract/edit embedded files",
-        "extract-images": "Extract images",
-        "extract-text":   "Extract text",
-        "imgtopdf":       "Convert images to PDF",
-        "pageobjects":    "Print info on pageobjects",
-        "pdfinfo":        "Print info on document and pages",
-        "fonts":          "List a document's fonts",
-        "render":         "Rasterize pages",
-        "tile":           "Tile pages (N-up)",
-        "toc":            "Print table of contents",
-    }
-    # TODO Only import the module that is actually used, see below.
-    CmdToModule = {n: importlib.import_module(f"pypdfium2_cli.{n.replace('-', '_')}") for n in SubCommands}
+    return f"pypdfium2 {PYPDFIUM_INFO}\npdfium {PDFIUM_INFO} at {_libs['pdfium']._name}"
 
 
-def get_parser():
+def get_parser(argv):
     
     main_parser = argparse.ArgumentParser(
         prog = "pypdfium2",
@@ -55,38 +51,41 @@ Environment variables:
   Whether to install a sysfont listener.\
 """ % dict(py_exe=basename(sys.executable)),
     )
-    main_parser.add_argument(
-        "--version", "-v",
-        action = "version",
-        version = f"pypdfium2 {PYPDFIUM_INFO}\n" f"pdfium {PDFIUM_INFO} at {_libs['pdfium']._name}"
-    )
     subparsers = main_parser.add_subparsers(dest="subcommand")
     
-    # TODO Only import and attach the module that is actually used. For main help, just add the subparsers without actually importing/attaching the modules.
-    for name, help in SubCommands.items():
-        mod = CmdToModule[name]
+    mod = None
+    sc_name = (argv and argv[0]) or None
+    if sc_name in (None, "-h", "--help"):
+        main_parser.add_argument("-v", "--version", action="version", version="")
+        for name, help in SubCommands.items():
+            subparsers.add_parser(name, help=help)
+    elif sc_name in ("-v", "--version"):
+        main_parser.add_argument("-v", "--version", action="version", version=get_version())
+    else:
+        help = SubCommands[sc_name]
+        # TODO use lazy dictionary (defaultdict) for imports
+        mod = importlib.import_module(f"pypdfium2_cli.{sc_name.replace('-', '_')}")
         desc = getattr(mod, "PARSER_DESC", None)
         desc = (help + "\n\n" + desc) if desc else help
         subparser = subparsers.add_parser(
-            name, help=help, description=desc,
+            sc_name, help=help, description=desc,
             formatter_class=argparse.RawTextHelpFormatter,
         )
         mod.attach(subparser)
     
-    return main_parser
+    return main_parser, mod
 
 
-def api_main(raw_args=sys.argv[1:]):
+def api_main(argv=sys.argv[1:]):
     
-    init()
-    parser = get_parser()
-    args = parser.parse_args(raw_args)
+    parser, mod = get_parser(argv)
+    args = parser.parse_args(argv)
     
     if not args.subcommand:
         parser.print_help()
         return
     
-    CmdToModule[args.subcommand].main(args)
+    mod.main(args)
 
 
 def cli_main():
