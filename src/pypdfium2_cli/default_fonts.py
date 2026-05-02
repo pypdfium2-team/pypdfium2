518b741002c61e1f239f522d3e4561d5f4055d70
geisserml <geisserml@gmail.com>
2026-05-02 15:11:43 +0200

    continue

--- src/pypdfium2_cli/default_fonts.py
+++ src/pypdfium2_cli/default_fonts.py
@@ -4,6 +4,7 @@
 import pypdfium2._helpers as pdfium
 import pypdfium2.internal as pdfium_i
 from pypdfium2_cli.fonts import _show_fonts
+# from ctypes import byref, c_int
 
 def attach(parser):
     pass
@@ -15,12 +16,20 @@ def _iterate_standard_fonts():
         yield fontobj.get_base_name(), fontobj.get_family_name()
 
 def main(args):
+    
     print("# Default TTF map")
+    ttfmap = pdfium.PdfDefaultTTFMap.value
     print(f"All Charsets: {sorted(pdfium_i.CharsetToStr.values())}")
-    missing = set(pdfium_i.CharsetToStr.keys()).difference(pdfium.PdfDefaultTTFMap.value.keys())
+    missing = set(pdfium_i.CharsetToStr.keys()).difference(ttfmap.keys())
     missing = [pdfium_i.CharsetToStr[k] for k in missing]
     print(f"Absent from map: {missing}")
-    str_ttfmap = {pdfium_i.CharsetToStr[k]: v for k, v in pdfium.PdfDefaultTTFMap.value.items()}
+    str_ttfmap = {pdfium_i.CharsetToStr[k]: v for k, v in ttfmap.items()}
     _show_fonts(("Charset", "Default font"), sorted(str_ttfmap.items()))
+    
+    # sfh = pdfium.PdfSysfontBase.SINGLETON
+    # if sfh:
+    #     for charset, fontname in ttfmap.items():
+    #         sfh.MapFont(None, weight=0, bItalic=False, charset=charset, pitch_family=0, face=fontname, _ignored=byref(c_int(0)))
+    
     print("\n# Standard fonts")
     _show_fonts(("Base name", "Family name"), _iterate_standard_fonts(), None)
