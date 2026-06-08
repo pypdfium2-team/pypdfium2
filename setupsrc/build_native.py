dc05dd4e18b7e1bb5fcb899a563621dc732883df
mara004 <geisserml@gmail.com>
2026-06-08 14:15:04 +0200

    Align libpng patch with our upstream CL (#441)
    https://chromium-review.googlesource.com/c/chromium/src/+/7903266

--- setupsrc/build_native.py
+++ setupsrc/build_native.py
@@ -247,8 +247,8 @@ def get_sources(deps_info, short_ver, with_tests, compiler, clang_ver, clang_pat
         git_apply_patch(PatchDir/"gcc_toolchain.patch", cwd=PDFIUM_DIR_build)
         if IS_ANDROID:  # fix linkage step
             git_apply_patch(PatchDir/"android_build.patch", cwd=PDFIUM_DIR_build)
-        if Host._raw_machine in ("loong64", "loongarch64"):
-            git_apply_patch(PatchDir/"loong64_use_lsx.patch", cwd=PDFIUM_DIR_build)  # for libpng
+        if Host._raw_machine in ("loong64", "loongarch64"):  # for libpng, non-essential
+            git_apply_patch(PatchDir/"loong64_use_lsx.patch", cwd=PDFIUM_DIR_build)
         if compiler is Compiler.clang:
             if clang_ver < 23:
                 git_apply_patch(PatchDir/"clang_22_compat.patch", cwd=PDFIUM_DIR_build)
@@ -310,8 +310,8 @@ def get_sources(deps_info, short_ver, with_tests, compiler, clang_ver, clang_pat
         df.fetch("nasm_source", PDFIUM_3RDPARTY/"nasm")
     if "libpng" in vendor_deps:
         do_patches = df.fetch("libpng", PDFIUM_3RDPARTY/"libpng", reset=reset)
-        if do_patches and Host._raw_machine == "ppc64le":
-            git_apply_patch(PatchDir/"libpng_ppc64.patch", cwd=PDFIUM_3RDPARTY/"libpng")
+        if do_patches and Host._raw_machine in ("ppc64le", "loong64", "loongarch64"):
+            git_apply_patch(PatchDir/"libpng_ppc64_loong64.patch", cwd=PDFIUM_3RDPARTY/"libpng")
     if "zlib" in vendor_deps:
         df.fetch("zlib", PDFIUM_3RDPARTY/"zlib")
     if "harfbuzz" in vendor_deps:
