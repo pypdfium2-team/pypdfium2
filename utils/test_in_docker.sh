cbd4cd9554a82afb9445b02815b19342c426aa52
geisserml <geisserml@gmail.com>
2026-06-17 23:41:58 +0200

    test_in_docker.sh: add `set -exuo pipefail`

--- utils/test_in_docker.sh
+++ utils/test_in_docker.sh
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2026 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
+set -exuo pipefail
+
 eval "$PREPARE_CMD"
 # alternatively: python3 -m pip config set global.break-system-packages true
 python3 -m venv --system-site-packages testenv
