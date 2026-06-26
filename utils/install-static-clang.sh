e63fcd0e5d713f6bae5bc02a5d672de09664967d
geisserml <geisserml@gmail.com>
2026-06-26 17:03:36 +0200

    install-static-clang: style improvements, prefer space over tab

--- utils/install-static-clang.sh
+++ utils/install-static-clang.sh
@@ -14,11 +14,11 @@ while getopts "d:m:" OPTION
 do
   case $OPTION in
     d)
-		STAGING_DIR="$(realpath $OPTARG)"
-		echo "download dir: $STAGING_DIR";;
-	m)
-		SCRIPT_MODE="$OPTARG"
-		echo "mode: $SCRIPT_MODE";;
+      STAGING_DIR="$(realpath $OPTARG)"
+      echo "download dir: $STAGING_DIR";;
+    m)
+      SCRIPT_MODE="$OPTARG"
+      echo "mode: $SCRIPT_MODE";;
     *)
       echo "Invalid flag -$OPTION"
       exit 1
@@ -29,25 +29,25 @@ done
 # Download static-clang
 DEFAULT_ARCH="$(uname -m)"
 if [ "${STATIC_CLANG_ARCH:-}" == "" ]; then
-	STATIC_CLANG_ARCH="${RUNNER_ARCH:-${DEFAULT_ARCH}}"
+    STATIC_CLANG_ARCH="${RUNNER_ARCH:-${DEFAULT_ARCH}}"
 fi
 case "${STATIC_CLANG_ARCH}" in
-	ARM64|aarch64|arm64|arm64/*) GO_ARCH=arm64;;
-	ARM|armv7l|armv8l|arm|arm/v7) GO_ARCH=arm;;  # assume arm/v7 for arm
-	X64|x86_64|amd64|amd64/*) GO_ARCH=amd64;;
-	X86|i686|386) GO_ARCH=386;;
-	loongarch64) GO_ARCH=loong64;;
-	ppc64le) GO_ARCH=ppc64le;;
-	riscv64) GO_ARCH=riscv64;;
-	s390x) GO_ARCH=s390x;;
-	*) echo "No static-clang toolchain for ${CLANG_ARCH}">2; exit 1;;
+    ARM64|aarch64|arm64|arm64/*) GO_ARCH=arm64;;
+    ARM|armv7l|armv8l|arm|arm/v7) GO_ARCH=arm;;  # assume arm/v7 for arm
+    X64|x86_64|amd64|amd64/*) GO_ARCH=amd64;;
+    X86|i686|386) GO_ARCH=386;;
+    loongarch64) GO_ARCH=loong64;;
+    ppc64le) GO_ARCH=ppc64le;;
+    riscv64) GO_ARCH=riscv64;;
+    s390x) GO_ARCH=s390x;;
+    *) echo "No static-clang toolchain for ${CLANG_ARCH}">2; exit 1;;
 esac
 
 STATIC_CLANG_VERSION=latest  # or pin 22.1.8.0
 if [ "${STATIC_CLANG_VERSION}" == "latest" ]; then
-	STATIC_CLANG_BASEURL="https://github.com/mayeut/static-clang-images/releases/latest/download"
+    STATIC_CLANG_BASEURL="https://github.com/mayeut/static-clang-images/releases/latest/download"
 else
-	STATIC_CLANG_BASEURL="https://github.com/mayeut/static-clang-images/releases/download/v${STATIC_CLANG_VERSION}"
+    STATIC_CLANG_BASEURL="https://github.com/mayeut/static-clang-images/releases/download/v${STATIC_CLANG_VERSION}"
 fi
 
 STATIC_CLANG_FILENAME="static-clang-linux-${GO_ARCH}.tar.xz"
@@ -61,7 +61,7 @@ mkdir -p "$STAGING_DIR"
 pushd "$STAGING_DIR"
 
 if [[ "$SCRIPT_MODE" == "skip-download" ]]; then
-	echo "skip-download mode"
+    echo "skip-download mode"
 else
     curl -fsSLO "$STATIC_CLANG_URL"
     curl -fsSL "$SHASUMS_URL" | grep "$STATIC_CLANG_FILENAME" > "$SHASUMS_NAME"
@@ -72,9 +72,8 @@ else
 fi
 
 if [[ "$SCRIPT_MODE" == "download-only" ]]; then
-echo "download-only mode"
-rm "$ATTEST_NAME" "$SHASUMS_NAME"
-exit 0
+    rm "$ATTEST_NAME" "$SHASUMS_NAME"
+    echo "download-only mode"; exit 0
 fi
 
 TOOLCHAIN_PATH="/opt/clang"
@@ -83,45 +82,44 @@ ln -s "${TOOLCHAIN_PATH}/bin/readelf" "${TOOLCHAIN_PATH}/bin/llvm-readelf"
 popd
 
 if [[ "$SCRIPT_MODE" == "install-only" ]]; then
-echo "install-only mode"
-exit 0
+    echo "install-only mode"; exit 0
 fi
 
 # configure target triple
 case "${AUDITWHEEL_POLICY}-${AUDITWHEEL_ARCH}" in
-	manylinux*-armv7l) TARGET_TRIPLE=armv7-unknown-linux-gnueabihf;;
-	musllinux*-armv7l) TARGET_TRIPLE=armv7-alpine-linux-musleabihf;;
-	manylinux*-ppc64le) TARGET_TRIPLE=powerpc64le-unknown-linux-gnu;;
-	musllinux*-ppc64le) TARGET_TRIPLE=powerpc64le-alpine-linux-musl;;
-	manylinux*-*) TARGET_TRIPLE=${AUDITWHEEL_ARCH}-unknown-linux-gnu;;
-	musllinux*-*) TARGET_TRIPLE=${AUDITWHEEL_ARCH}-alpine-linux-musl;;
+    manylinux*-armv7l) TARGET_TRIPLE=armv7-unknown-linux-gnueabihf;;
+    musllinux*-armv7l) TARGET_TRIPLE=armv7-alpine-linux-musleabihf;;
+    manylinux*-ppc64le) TARGET_TRIPLE=powerpc64le-unknown-linux-gnu;;
+    musllinux*-ppc64le) TARGET_TRIPLE=powerpc64le-alpine-linux-musl;;
+    manylinux*-*) TARGET_TRIPLE=${AUDITWHEEL_ARCH}-unknown-linux-gnu;;
+    musllinux*-*) TARGET_TRIPLE=${AUDITWHEEL_ARCH}-alpine-linux-musl;;
 esac
 case "${AUDITWHEEL_POLICY}-${AUDITWHEEL_ARCH}" in
-	*-riscv64) M_ARCH="-march=rv64gc";;
-	*-x86_64) M_ARCH="-march=x86-64";;
-	*-armv7l) M_ARCH="-march=armv7a";;
-	manylinux*-i686) M_ARCH="-march=k8 -mtune=generic";;  # same as gcc manylinux2014 / manylinux_2_28
-	musllinux*-i686) M_ARCH="-march=pentium-m -mtune=generic";;  # same as gcc musllinux_1_2
+    *-riscv64) M_ARCH="-march=rv64gc";;
+    *-x86_64) M_ARCH="-march=x86-64";;
+    *-armv7l) M_ARCH="-march=armv7a";;
+    manylinux*-i686) M_ARCH="-march=k8 -mtune=generic";;  # same as gcc manylinux2014 / manylinux_2_28
+    musllinux*-i686) M_ARCH="-march=pentium-m -mtune=generic";;  # same as gcc musllinux_1_2
 esac
 GCC_TRIPLE=$(gcc -dumpmachine)
 
 cat<<EOF >"${TOOLCHAIN_PATH}/bin/${AUDITWHEEL_PLAT}.cfg"
-	-target ${TARGET_TRIPLE}
-	${M_ARCH:-}
-	--gcc-toolchain=${DEVTOOLSET_ROOTPATH:-}/usr
-	--gcc-triple=${GCC_TRIPLE}
+-target ${TARGET_TRIPLE}
+${M_ARCH:-}
+--gcc-toolchain=${DEVTOOLSET_ROOTPATH:-}/usr
+--gcc-triple=${GCC_TRIPLE}
 EOF
 
 cat<<EOF >"${TOOLCHAIN_PATH}/bin/clang.cfg"
-	@${AUDITWHEEL_PLAT}.cfg
+@${AUDITWHEEL_PLAT}.cfg
 EOF
 
 cat<<EOF >"${TOOLCHAIN_PATH}/bin/clang++.cfg"
-	@${AUDITWHEEL_PLAT}.cfg
+@${AUDITWHEEL_PLAT}.cfg
 EOF
 
 cat<<EOF >"${TOOLCHAIN_PATH}/bin/clang-cpp.cfg"
-	@${AUDITWHEEL_PLAT}.cfg
+@${AUDITWHEEL_PLAT}.cfg
 EOF
 
 # override entrypoint to add the toolchain to PATH
