fe58d19c28f3d6978e7ca8b963ccafab7595e02b
geisserml <geisserml@gmail.com>
2026-06-27 19:21:50 +0200

    add note

--- strategy/get_matrix.py
+++ strategy/get_matrix.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2026 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
+# To keep this script as swift as possible, we endeavor not to import from setupsrc/ here
+
 import sys
 import json
 import argparse
