7b904dae520da7e6c08b1015524f24081550780e
geisserml <geisserml@gmail.com>
2026-06-30 15:59:15 +0200

    Inline call_container_driver.sh

--- utils/call_container_driver.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-set -exuo pipefail
-
-# q&d: infer container target name from wheel name
-UTILS_DIR="$(dirname -- "$0")"
-WHEEL_PATH="$1"
-TARGET=$(python3 "$UTILS_DIR/regextract.py" "-(\w+linux)_[\d_]+_(\w+)\." "%s_%s" "$(ls $WHEEL_PATH)")
-echo "$TARGET"
-python3 "$UTILS_DIR/container_driver.py" "$TARGET" -w $WHEEL_PATH
