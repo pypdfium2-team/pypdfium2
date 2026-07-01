ee065a0825433462634c270d5d78af6ac03825c6
geisserml <geisserml@gmail.com>
2026-07-01 10:29:33 +0200

    Use a python script for testing multiple python versions

---
+++ utils/multitest.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+# SPDX-FileCopyrightText: 2026 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
+import sys
+import shlex
+import argparse
+import subprocess
+from pathlib import Path
+
+UTILS_DIR = Path(__file__).resolve().parent
+PROJECT_DIR = UTILS_DIR.parent
+
+def log(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
+
+parser = argparse.ArgumentParser(
+    description="Test with multiple python versions"
+)
+parser.add_argument("-w", "--wheel-path", required=True)
+parser.add_argument("--py-vers", nargs="+", required=True)
+parser.add_argument("--prefix")
+parser.add_argument("--venv", action="store_true")
+args = parser.parse_args()
+
+archprefix = []
+if args.prefix:
+    archprefix = shlex.split(args.prefix)
+
+def run(cmd):
+    cmd = archprefix + cmd
+    log(cmd)
+    subprocess.run(cmd, check=True, cwd=PROJECT_DIR)
+
+errors = {}
+for py_ver in reversed(args.py_vers):
+    python = f"python{py_ver}"
+    if args.venv:
+        venv_name = f"testenv_{py_ver}"
+        run([python, "-m", "venv", venv_name])
+        python = str(Path(venv_name)/"bin"/"python")
+    if archprefix:
+        run([python, "-c", "import platform as p; print(p.machine())"])
+    run([python, "-m", "pip", "install", args.wheel_path])
+    run([python, "-m", "pip", "install", "-U", "-r", "req/test.txt"])
+    try:
+        run([python, "-m", "pypdfium2_cli", "--version"])
+        run([python, "-m", "pytest", "tests/", "-sv"])
+    except subprocess.CalledProcessError as e:
+        errors[py_ver] = e.returncode
+    else:
+        errors[py_ver] = 0
+
+log(errors)
+if any(e != 0 for e in errors.values()):
+    sys.exit(1)
--- utils/test_scriptlet.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-set -exuo pipefail
-
-PYTHON="$1"
-
-$PYTHON -m pip install dist/*.whl
-$PYTHON -m pip install -U -r req/test.txt
-$PYTHON -m pypdfium2_cli --version
-$PYTHON -m pytest tests/ -sv
