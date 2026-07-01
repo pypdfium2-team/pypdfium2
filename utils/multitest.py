aad9c15e2455acf455e17069ae79801a1dba3f77
geisserml <geisserml@gmail.com>
2026-07-01 11:11:29 +0200

    add a comment

--- utils/multitest.py
+++ utils/multitest.py
@@ -2,6 +2,8 @@
 # SPDX-FileCopyrightText: 2026 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
+# Author's note: I'm starting to really see what cibuildwheel exists for. This basically replicates a small chunk of what cibuildwheel does internally.
+
 import sys
 import shlex
 import argparse
