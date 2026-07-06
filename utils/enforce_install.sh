15c85a73e161f266808f14daa7058ba02462687a
geisserml <geisserml@gmail.com>
2026-07-06 17:01:16 +0200

    Fix enforce_install utility

--- utils/enforce_install.sh
+++ utils/enforce_install.sh
@@ -6,6 +6,6 @@ WHEEL_PATH="$1"
 PYTHON="$2"
 WHEEL_DIR=$(dirname $WHEEL_PATH)
 
-HOST_PLATFORM=$($PYTHON -c "import sysconfig; print(sysconfig.get_platform())")
-RETAGGED_WHEEL_NAME=$($PYTHON -m wheel tags --platform-tag=linux_mips64 $WHEEL_PATH)
+HOST_PLATFORM=$($PYTHON -c "import sysconfig; print(sysconfig.get_platform().replace('-', '_'))")
+RETAGGED_WHEEL_NAME="$($PYTHON -m wheel tags --platform-tag="$HOST_PLATFORM" "$WHEEL_PATH")"
 $PYTHON -m pip install -v $WHEEL_DIR/$RETAGGED_WHEEL_NAME
