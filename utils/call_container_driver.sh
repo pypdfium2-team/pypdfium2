0c8852fdc0aa83c0fa8ff1153f4fb56efa19749b
geisserml <geisserml@gmail.com>
2026-07-07 18:24:47 +0200

    Inline call_container_driver back into test action

--- utils/call_container_driver.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-set -exuo pipefail
-
-# q&d: infer container target name from wheel name
-WHEEL_PATH="$1"
-TARGET="$(python3 utils/regextract.py "-(\w+linux)_[\d_]+_(\w+)\." "%s_%s" "$WHEEL_PATH")"
-python3 utils/container_driver.py "$TARGET" -w "$WHEEL_PATH"
