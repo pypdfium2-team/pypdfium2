ed6db0518bee2d6f3778fbde88a64e6a200cade7
geisserml <geisserml@gmail.com>
2026-07-19 21:10:41 +0200

    continue

--- src/pypdfium2_cfg/stl.py
+++ src/pypdfium2_cfg/stl.py
@@ -10,9 +10,9 @@ class cached_property:
         
         Though this implementation does not explicitly access ``__dict__``, attempts to use cached_property in a slotted class will fail just as the stdlib's, because attributes of a slotted class cannot be shadowed on instance level, which is essential for a decorator-based cached property that should assume zero overhead after the initial access.
         
-        While this is a limitation of Python's ``__slots__`` feature, you may be able to achieve the same goal through a different approach.
-        Your best bet is probably to implement a ``__getattr__`` method with a table of cached property names and functions, and add the names to the slots.
-        This should also be overhead-free, since once the slot is assigned ``__getattr__`` is no longer called, and you can use ``del`` likewise to unassign the slot and reroute access to ``__getattr__``.
+        While this is a limitation of Python's ``__slots__`` feature, it may be possible to achieve the same goal through a different approach.
+        We would probably try implementing a ``__getattr__`` method with a dict of cached property names and functions, and add the names to the slots.
+        This should also be overhead-free, since once the slot is assigned ``__getattr__`` would no longer be called, and you can use ``del`` likewise to unassign the slot and reroute access to ``__getattr__``.
     """
     
     def __init__(self, func):
