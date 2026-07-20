0ccf38922f331160cd64ce3076c8431ec05c889f
geisserml <geisserml@gmail.com>
2026-07-20 11:56:49 +0200

    continue

--- src/pypdfium2_stl/shared/stl.py
+++ src/pypdfium2_stl/shared/stl.py
@@ -13,8 +13,8 @@ class cached_property:
         With slots, you need to take a different approach that does not work with a decorator API: Add the property names to your slots and implement a ``__getattr__`` method which dispatches the cached property functions through a map or similar and assigns the result to the object. This is also zero overhead after load.
     
     Warning:
-        Don't be tempted to think you could implement a cached_property backport by stacking :class:`property` and :func:`functools.lru_cache`. You cannot. LRU cache operates on class level, not instance level, which has a ton of negative though not immediately obvious implications.
-        When setting a maxsize, caches will be lost once there are more instance objects. E.g. with maxsize=1 only one instance object (the most recently used) can have a cache, whereas any other cached values are silently lost and re-created on access. Without a maxsize, the cache will grow indefinitely, and mind the strong references it might hold!
+        Don't be tempted to think you could replace :class:`.cached_property` by stacking :class:`property` and :func:`functools.lru_cache`. You cannot. LRU cache operates on class level, not instance level, which has a ton of negative though not immediately obvious implications.
+        When setting a maxsize, caches will be lost once there are more instance objects. E.g. with maxsize=1 only one instance object (the most recently used) can have a cache, whereas any other cached values are silently lost and re-created on access. Without a maxsize, the cache will grow indefinitely, and mind the strong references causing your values to never be garbage collected!
     """
     
     def __init__(self, func):
