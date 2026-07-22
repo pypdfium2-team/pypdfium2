60846910af43bdcae182da12395f6fbd1d4f09a1
geisserml <geisserml@gmail.com>
2026-07-22 17:43:26 +0200

    Make container_driver script more powerful

--- utils/container_driver.py
+++ utils/container_driver.py
@@ -10,6 +10,7 @@ from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parents[1]/"setupsrc"))
 from base import ProjectDir, log  # local
 
+_RHEL_CMD   = "yum install -y python3 python3-virtualenv python3-pillow python3-numpy"  # python3-pytest
 _DEBIAN_CMD = "apt-get update && apt-get install --no-install-recommends -y python3 python3-pip python3-venv python3-pillow python3-numpy python3-pytest"
 _ALPINE_CMD = "apk add python3 py3-pip py3-pillow py3-numpy py3-pytest"
 
@@ -35,23 +36,20 @@ PLATFORM_CPU_MAP = {
 # loong64, mips64le, ppc64le, riscv64, s390x
 
 
-def _run_process(argv, **kwargs):
-    log(argv)
-    return subprocess.run(argv, **kwargs)
-
-
-def _get_container(os_class, cpu):
+def _get_container(image, os_class, cpu):
     docker_cpu = DOCKER_CPU_MAP.get(cpu, cpu)
     platform_cpu = PLATFORM_CPU_MAP.get(cpu, cpu)
     docker_flags = ("--platform", f"linux/{platform_cpu}")
-    if docker_cpu == "loong64":
-        prefix = f"ghcr.io/"
-    else:
-        prefix = ""
-    if os_class == "manylinux":
+    prefix = f"ghcr.io/" if docker_cpu == "loong64" else ""
+    if image == "debian":
+        assert os_class == "manylinux"
         which_debian = "bookworm" if docker_cpu == "mips64le" else "trixie"
         return f"{prefix}{docker_cpu}/debian:{which_debian}-slim", docker_flags, _DEBIAN_CMD, "bash"
-    elif os_class == "musllinux":
+    elif image == "manylinux2014":
+        assert os_class == "manylinux"
+        return f"quay.io/pypa/manylinux2014_{cpu}", docker_flags, _RHEL_CMD, "bash"
+    elif image == "alpine":
+        assert os_class == "musllinux"
         return f"{prefix}{docker_cpu}/alpine:3", docker_flags, _ALPINE_CMD, "sh"
     else:
         assert False, os_class
@@ -62,11 +60,42 @@ def parse_args():
         description = "Install and test pypdfium2 in docker container",
     )
     parser.add_argument("target")
-    parser.add_argument("-w", "--wheel-path", required=True)
+    parser.add_argument("--image")
+    parser.add_argument(
+        "-w", "--wheel-path",
+        type = lambda p: Path("/pypdfium2") / p,
+    )
     args = parser.parse_args(sys.argv[1:])
-    args.wheel_path = Path("/pypdfium2") / args.wheel_path
     return args
 
+
+SCRIPT_TEMPLATE = """\
+set -exuo pipefail
+
+%(install_pkgs)s
+VENV_DIR="/testenv"
+python3 -m venv "$VENV_DIR" --system-site-packages
+export PATH="$VENV_DIR/bin:$PATH"
+which python3
+python3 --version
+python3 -m pip install -U pip
+cd /pypdfium2
+%(install_lib)s
+pypdfium2
+python3 -m pytest tests/
+"""
+
+INSTALL_WHEEL = """
+python3 -m pip install "$1"
+"""
+INSTALL_WHEEL_BYFORCE = """
+python3 -m pip install -U wheel
+bash "/pypdfium2/utils/enforce_install.sh" "$1"
+"""
+INSTALL_FROM_SRC = """
+python3 -m pip install -U setuptools packaging wheel build pytest
+python3 -m pip install --no-build-isolation -v .
+"""
 
 def main():
     
@@ -77,13 +106,24 @@ def main():
     
     os_class, cpu = args.target.split("_", maxsplit=1)
     cpu = {"loongarch64": "loong64"}.get(cpu, cpu)
+    if not args.image:
+        args.image = {"manylinux": "debian", "musllinux": "alpine"}[os_class]
     
-    container, docker_flags, prepare_cmd, shell = _get_container(os_class, cpu)
-    log(f"{container}, {docker_flags}, {prepare_cmd}, {shell}")
+    container, docker_flags, install_pkgs, shell = _get_container(args.image, os_class, cpu)
+    if args.wheel_path:
+        # https://github.com/pypa/pip/issues/14095
+        install_lib = INSTALL_WHEEL if not cpu.startswith("mips") else INSTALL_WHEEL_BYFORCE
+    else:
+        install_lib = INSTALL_FROM_SRC
     
-    env = os.environ.copy()
-    env["PREPARE_CMD"] = prepare_cmd
-    _run_process(["docker", "run", "--security-opt", "label=disable", "-e", "PREPARE_CMD", "-i", "--rm", "-v", f"{ProjectDir}:/pypdfium2", *docker_flags, container, shell, "/pypdfium2/utils/inside_docker.sh", str(args.wheel_path)], cwd=ProjectDir, env=env, check=True)
+    script = SCRIPT_TEMPLATE % dict(install_pkgs=install_pkgs, install_lib=install_lib.strip())
+    docker_cmd = ["docker", "run", "--security-opt", "label=disable", "-i", "--rm", "-v", f"{ProjectDir}:/pypdfium2", *docker_flags, container, shell]
+    if args.wheel_path:
+        docker_cmd += ["--", args.wheel_path]
+    
+    log(docker_cmd)
+    log(script)
+    subprocess.run(docker_cmd, input=script.encode(), cwd=ProjectDir, check=True)
 
 
 if __name__ == "__main__":
--- utils/enforce_install.sh
+++ utils/enforce_install.sh
@@ -3,9 +3,8 @@
 set -exuo pipefail
 
 WHEEL_PATH="$1"
-PYTHON="$2"
 WHEEL_DIR=$(dirname $WHEEL_PATH)
 
-HOST_PLATFORM=$($PYTHON -c "import sysconfig; print(sysconfig.get_platform().replace('-', '_'))")
-RETAGGED_WHEEL_NAME=$($PYTHON -m wheel tags --platform-tag="$HOST_PLATFORM" "$WHEEL_PATH")
-$PYTHON -m pip install -v $WHEEL_DIR/$RETAGGED_WHEEL_NAME
+HOST_PLATFORM=$(python3 -c "import sysconfig; print(sysconfig.get_platform().replace('-', '_'))")
+RETAGGED_WHEEL_NAME=$(python3 -m wheel tags --platform-tag="$HOST_PLATFORM" "$WHEEL_PATH")
+python3 -m pip install -v $WHEEL_DIR/$RETAGGED_WHEEL_NAME
