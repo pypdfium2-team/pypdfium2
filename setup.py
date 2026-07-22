pypdfium2-team/pypdfium2

commit 6f552008fa186e789b7bceb885e36cb10eb227b9

Python 3.6 compat fixes (runtime & setup) (#465)

setup.py
@@ -1,7 +1,11 @@
 # SPDX-FileCopyrightText: 2026 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
-# See also https://stackoverflow.com/questions/45150304/how-to-force-a-python-wheel-to-be-platform-specific-when-building-it and https://github.com/innodatalabs/redstork/blob/master/setup.py
+# Background reading:
+# https://stackoverflow.com/questions/45150304/how-to-force-a-python-wheel-to-be-platform-specific-when-building-it
+# https://stackoverflow.com/questions/69647983/how-to-add-platform-specific-package-data-in-setup-py
+# https://github.com/tim-mitchell/prebuilt_binaries
+# https://cibuildwheel.pypa.io/en/stable/faq/#actions-you-need-to-perform-before-building
 
 import os
 import sys
@@ -23,6 +27,7 @@ from tagging import get_wheel_tag
 
 # Use a custom distclass declaring we have a binary extension, to prevent modules from being nested in a purelib/ subdirectory in wheels.
 # This will also set `Root-Is-Purelib: false` in the WHEEL file, and make the wheel tag platform specific by default.
+# FIXME For some reason, this does not work properly with Python 3.6 / older setuptools - we still get a purelib folder, even though Root-Is-Purelib is false. :(
 
 class BinaryDistribution (setuptools.Distribution):
     
@@ -32,21 +37,16 @@ class BinaryDistribution (setuptools.Distribution):
 
 def buildpy_factory(pl_name, modnames, datagen, helpers_info, package_data):
     
-    # https://cibuildwheel.pypa.io/en/stable/faq/#actions-you-need-to-perform-before-building
-    
     class pypdfium_buildpy (buildpy_orig):
         
         def run(self, *args, **kwargs):
-            
             if ModuleRaw in modnames and pl_name != ExtPlats.sdist:
                 datagen()
                 assert_exists(ModuleDir_Raw, package_data["pypdfium2_raw"])
-            
             if ModuleHelpers in modnames:
                 helpers_info["is_editable"] = getattr(self, "editable_mode", None)
                 write_json(ModuleDir_Helpers/VersionFN, helpers_info)
                 assert_exists(ModuleDir_Helpers, package_data["pypdfium2"])
-            
             buildpy_orig.run(self, *args, **kwargs)
     
     return pypdfium_buildpy
@@ -118,8 +118,7 @@ def run_setup(modnames, pl_name, datagen):
         name = "pypdfium2",
         description = "Python bindings to PDFium",
         license = "BSD-3-Clause, Apache-2.0, dependency licenses",
-        # TODO Fix and re-enable Python >= 3.6 (see the note in src/pypdfium2/_lazy.py for why it is broken)
-        python_requires = ">= 3.8",
+        python_requires = ">= 3.6",
         cmdclass = {},
         package_dir = {},
         package_data = {},
@@ -158,8 +157,8 @@ def run_setup(modnames, pl_name, datagen):
         helpers_info = _get_fixed_helpers_info(pl_name)
         kwargs["version"] = merge_tag(helpers_info, mode="py")
         kwargs["package_dir"]["pypdfium2"] = "src/pypdfium2"
-        kwargs["package_dir"]["pypdfium2_cli"] = "src/pypdfium2_cli"
-        kwargs["package_dir"]["pypdfium2_cfg"] = "src/pypdfium2_cfg"
+        for sm in ("cli", "cfg", "stl"):
+            kwargs["package_dir"][f"pypdfium2_{sm}"] = f"src/pypdfium2_{sm}"
         kwargs["package_data"]["pypdfium2"] = (VersionFN, )
         kwargs["entry_points"] = dict(console_scripts=["pypdfium2 = pypdfium2_cli.__main__:cli_main"])
     if ModuleRaw in modnames:
@@ -188,6 +187,10 @@ def run_setup(modnames, pl_name, datagen):
     
     kwargs["cmdclass"]["build_py"] = buildpy_factory(pl_name, modnames, datagen, helpers_info, kwargs["package_data"])
     kwargs["license_files"] = license_files
+    
+    # An explicit package finder is required for older versions of Python which are stuck with older setuptools (e.g. Python 3.6 has max setuptools 59.6.0).
+    # Note that this finder cannot be moved to pyproject.toml because older setuptools do not look for it there yet, whereas with newer setuptools (>= 61) this could just be omitted entirely thanks to auto-discovery.
+    kwargs["packages"] = setuptools.find_packages(where='src', include=['pypdfium2*'])
     
     setuptools.setup(**kwargs)
 

pyproject.toml
@@ -4,6 +4,7 @@
 [build-system]
 build-backend = "setuptools.build_meta"
 requires = [
+    # We do not declare an explicit setuptools min version for a variety of reasons. Last time it was tested, setuptools 59 (max with Python 3.6) still kind of worked.
     "setuptools",
     "packaging",
     # XXX dependence on wheel is historical, with setuptools >= v70.1.0 this is not needed anymore

setup.cfg
@@ -1,7 +1,8 @@
 # SPDX-FileCopyrightText: 2026 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
-# NOTE We have to use setup.cfg rather than pyproject.toml to declare static metadata because the latter does not permit a dynamic project name.
+# Note, we have to use setup.cfg rather than pyproject.toml to declare static metadata because the latter does not permit a dynamic project name.
+# TODO(geisserml) Consider inlining this into setup.py after all, to avoid too many individual configuration files, which adds fragmentation and complexity.
 
 # NOTE We use requirements files instead of [options.extras_require]. Rationale:
 # - BUG(177): PyPI refuses upload if custom deps ($DEP @ git+$URL) are specified.

setupsrc/base.py
@@ -17,12 +17,11 @@ from pathlib import Path
 from collections import namedtuple
 import urllib.request as url_request
 
-if sys.version_info < (3, 8):
-    # NOTE alternatively, we could write our own cached property backport with python's descriptor protocol
-    def cached_property(func):
-        return property( functools.lru_cache(maxsize=1)(func) )
-else:
-    cached_property = functools.cached_property
+ProjectDir = Path(__file__).resolve().parents[1]
+
+# Add this path to your IDE's search path, e.g. VS Code python.analysis.extraPaths
+sys.path.insert(0, str(ProjectDir/"src"/"pypdfium2_stl"/"shared"))
+from stl import cached_property
 
 if sys.version_info < (3, 8):
     class ExtendAction (argparse.Action):
@@ -60,7 +59,6 @@ ModulesAll         = (ModuleRaw, ModuleHelpers)
 BindingsFN = "bindings.py"
 VersionFN  = "version.json"
 
-ProjectDir        = Path(__file__).resolve().parents[1]
 DataDir           = ProjectDir / "data"
 DataDir_Bindings  = DataDir / "bindings"
 BindingsFile      = DataDir_Bindings / BindingsFN

src/pypdfium2/_helpers/pageobjects.py
@@ -15,7 +15,8 @@ from pypdfium2.internal import FPDF_WCHAR_size
 from pypdfium2._helpers.misc import PdfiumError
 from pypdfium2._helpers.matrix import PdfMatrix
 from pypdfium2._helpers.bitmap import PdfBitmap
-from pypdfium2._lazy import Lazy, cached_property
+from pypdfium2._lazy import Lazy
+from pypdfium2_stl import cached_property
 
 logger = logging.getLogger(__name__)
 

src/pypdfium2/_helpers/sysfontinfo.py
@@ -10,7 +10,7 @@ import logging
 import pypdfium2.raw as pdfium_c
 import pypdfium2.internal as pdfium_i
 from pypdfium2._helpers.misc import PdfiumError
-from pypdfium2._lazy import cached_property, cached_property_clear
+from pypdfium2_stl import cached_property
 
 logger = logging.getLogger(__name__)
 
@@ -38,7 +38,7 @@ class _DefaultSysfontInfoClass (pdfium_i.AutoCastable):
             return
         pdfium_i._debug_close("Free default sysfont info")
         pdfium_c.FPDF_FreeDefaultSystemFontInfo(self.raw)
-        cached_property_clear(self, "raw")
+        del self.raw
         pdfium_i.ObjectTracker[None].remove(self._wref_to_self)
         self._is_loaded = False
     

src/pypdfium2/_helpers/textpage.py
@@ -10,7 +10,7 @@ import pypdfium2.raw as pdfium_c
 import pypdfium2.internal as pdfium_i
 from pypdfium2._helpers.misc import PdfiumError
 from pypdfium2._helpers.pageobjects import PdfTextObj
-from pypdfium2._lazy import cached_property
+from pypdfium2_stl import cached_property
 
 c_double = ctypes.c_double
 logger = logging.getLogger(__name__)

src/pypdfium2/_lazy.py
@@ -3,25 +3,10 @@
 
 # see https://gist.github.com/mara004/6915e904797916b961e9c53b4fc874ec for alternative approaches to deferred imports
 
-import sys
 import logging
-import functools
+from pypdfium2_stl import cached_property
 
 logger = logging.getLogger(__name__)
-
-if sys.version_info < (3, 8):  # pragma: no cover
-    # Alternatively, we could write our own cached property backport with python's descriptor protocol
-    # -> FIXME For some reason, functools class-level cache breaks our autoclose machinery! This means pypdfium2 is currently broken on Python < 3.8. So yes, we definitely need our own instance-level backport.
-    def cached_property(func):
-        return property( functools.lru_cache(maxsize=1)(func) )
-    
-    def cached_property_clear(obj, name):
-        getattr(type(obj), name).fget.cache_clear()
-
-else:
-    cached_property = functools.cached_property
-    def cached_property_clear(obj, name):
-        delattr(obj, name)
 
 class _LazyClass:
     

commit 26eaa4e413cf33c36ebe103044d773f89afd4390

setup.py: fix broken PYPDFIUM_MODULES={raw,helpers} install
Accidentally broken by 6f552008fa186e789b7bceb885e36cb10eb227b9

setup.py
@@ -137,10 +137,13 @@ def run_setup(modnames, pl_name, datagen):
     if pl_name == ExtPlats.sdist:
         license_files.extend(LICENSES_SDIST)
     
+    exclude_rules = []
     if modnames == [ModuleHelpers]:
         kwargs["name"] += "_helpers"
         kwargs["description"] += " (helpers module)"
         kwargs["install_requires"] = ["pypdfium2_raw"]
+        include_rules = ["pypdfium2*"]
+        exclude_rules.append("pypdfium2_raw*")
     elif modnames == [ModuleRaw]:
         kwargs["name"] += "_raw"
         kwargs["description"] += " (raw module)"
@@ -148,9 +151,9 @@ def run_setup(modnames, pl_name, datagen):
         log(f"Warning: PYPDFIUM_MODULES=raw expects pre-generated version file at src/pypdfium2_raw/{VersionFN}. Callers must ensure this matches the version being built!")
         pdfium_fullver, _ = read_pdfium_info(ModuleDir_Raw)
         kwargs["version"] = str(pdfium_fullver)
+        include_rules = ["pypdfium2_raw*"]
     else:
-        assert any(m in modnames for m in (ModuleHelpers, ModuleRaw)), \
-               f"At least one core module is required. Check {ModulesSpec_EnvVar}."
+        include_rules = ["pypdfium2*"]
     
     helpers_info = None
     if ModuleHelpers in modnames:
@@ -171,17 +174,14 @@ def run_setup(modnames, pl_name, datagen):
     elif pl_name == ExtPlats.system:
         kwargs["exclude_package_data"] = {"pypdfium2_raw": LIBNAME_GLOBS}
     else:
-        
         if pl_name == ExtPlats.sourcebuild:
             use_tarball_licenses = False
         else:  # pdfium-binaries
             use_tarball_licenses = bool(int( os.getenv("USE_TARBALL_LICENSES", 0) ))
-        
         if use_tarball_licenses:
             license_files.append(f"data/{pl_name}/BUILD_LICENSES/**")
         else:
             license_files.append("BUILD_LICENSES/**")
-        
         kwargs["distclass"] = BinaryDistribution
         kwargs["cmdclass"]["bdist_wheel"] = bdist_factory(pl_name, dll_path)
     
@@ -190,7 +190,8 @@ def run_setup(modnames, pl_name, datagen):
     
     # An explicit package finder is required for older versions of Python which are stuck with older setuptools (e.g. Python 3.6 has max setuptools 59.6.0).
     # Note that this finder cannot be moved to pyproject.toml because older setuptools do not look for it there yet, whereas with newer setuptools (>= 61) this could just be omitted entirely thanks to auto-discovery.
-    kwargs["packages"] = setuptools.find_packages(where='src', include=['pypdfium2*'])
+    # kwargs["include_package_data"] = True
+    kwargs["packages"] = setuptools.find_packages(where='src', include=include_rules, exclude=exclude_rules)
     
     setuptools.setup(**kwargs)
 