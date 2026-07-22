a53429a540b89dfe7b7e40148157819279109ba0
geisserml <geisserml@gmail.com>
2026-07-22 22:07:29 +0200

    Avoid deprecated practice (direct CLI use of pypdfium2 module)

--- src/pypdfium2/__main__.py
+++ src/pypdfium2/__main__.py
@@ -6,12 +6,12 @@ import os.path
 import warnings
 from pypdfium2_cli.__main__ import *
 
-_DEPRECATION_REASON = " Using a separate submodule is necessary to allow for proper preparation before library init (e.g. set up logging), since a module's __main__.py implies its __init__.py."
+_DEPRECATION_REASON = "Using a separate submodule is necessary to allow for proper preparation before library init (e.g. set up logging), since a module's __main__.py implies its __init__.py."
 
 if __name__ == "__main__":
     _py_exe = os.path.basename(sys.executable)
     warnings.simplefilter("always")
-    warnings.warn(f"`{_py_exe} -m pypdfium2` is deprecated. Use `{_py_exe} -m pypdfium2_cli` or the `pypdfium2` entrypoint script instead."+_DEPRECATION_REASON, category=DeprecationWarning, stacklevel=2)
+    warnings.warn(f"`{_py_exe} -m pypdfium2` is deprecated. Use `{_py_exe} -m pypdfium2_cli` or the `pypdfium2` entrypoint script instead. " + _DEPRECATION_REASON, category=DeprecationWarning, stacklevel=2)
     cli_main()
 else:
-    warnings.warn("Importing pypdfium2.__main__ is deprecated. Use pypdfium2_cli.__main__ instead."+_DEPRECATION_REASON, category=DeprecationWarning, stacklevel=2)
+    warnings.warn("Importing pypdfium2.__main__ is deprecated. Use pypdfium2_cli.__main__ instead. " + _DEPRECATION_REASON, category=DeprecationWarning, stacklevel=2)
