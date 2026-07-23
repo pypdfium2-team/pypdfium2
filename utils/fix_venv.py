81e45c00c88283a11d51020f9d15caf62f28cd07
geisserml <geisserml@gmail.com>
2026-07-23 22:02:36 +0200

    continue

--- utils/fix_venv.py
+++ utils/fix_venv.py
@@ -4,15 +4,20 @@
 import sys
 from pathlib import Path
 
-VENV_BIN_DIR = Path(sys.argv[1]).resolve()
-assert VENV_BIN_DIR.exists()
+VENV_BIN = Path(sys.argv[1])
+assert VENV_BIN.is_absolute(), "VENV_BIN must be absolute"
+assert VENV_BIN.exists(), "VENV_BIN must exist"
 
 if sys.platform.startswith("win32"):
-    python_exe = VENV_BIN_DIR/"python.exe"
-    pip_exe = VENV_BIN_DIR/"pip.exe"
-    python3_exe = VENV_BIN_DIR/"python3.exe"
-    pip3_exe = VENV_BIN_DIR/"pip3.exe"
-    assert python_exe.exists() and pip_exe.exists()
+    
+    python_exe = VENV_BIN/"python.exe"
+    pip_exe = VENV_BIN/"pip.exe"
+    python3_exe = VENV_BIN/"python3.exe"
+    pip3_exe = VENV_BIN/"pip3.exe"
+    
+    assert python_exe.exists(), "python.exe must exist"
+    assert pip_exe.exists(), "pip.exe must exist"
+    
     if not python3_exe.exists():
         python3_exe.symlink_to(python_exe)
     if not pip3_exe.exists():
