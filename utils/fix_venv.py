81a9c0868fe8e9cc70a9a135b21cf5f1e043a1bd
geisserml <geisserml@gmail.com>
2026-07-24 20:12:07 +0200

    Delegate VENV_BIN resolution to fix_venv.py script

--- utils/fix_venv.py
+++ utils/fix_venv.py
@@ -1,12 +1,13 @@
 import sys
 from pathlib import Path
 
-VENV_BIN = Path(sys.argv[1])
-assert VENV_BIN.is_absolute(), "VENV_BIN must be absolute"
-assert VENV_BIN.exists(), "VENV_BIN must exist"
+VENV_ROOT = Path(sys.argv[1]).resolve()
+IS_WINDOWS = sys.platform.startswith("win32")
+VENV_BIN = VENV_ROOT / ("Scripts" if IS_WINDOWS else "bin")
+assert VENV_ROOT.exists(), f"{VENV_ROOT} does not exist"
+assert VENV_BIN.exists(), f"{VENV_BIN} does not exist"
 
-if sys.platform.startswith("win32"):
-    
+if IS_WINDOWS:
     python_exe = VENV_BIN/"python.exe"
     pip_exe = VENV_BIN/"pip.exe"
     python3_exe = VENV_BIN/"python3.exe"
@@ -19,3 +20,5 @@ if sys.platform.startswith("win32"):
         python3_exe.symlink_to(python_exe)
     if not pip3_exe.exists():
         pip3_exe.symlink_to(pip_exe)
+
+print(str(VENV_BIN), end="")
