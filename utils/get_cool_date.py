1f47b3675334725e9257c7d288ec830918b01011
geisserml <geisserml@gmail.com>
2026-07-24 12:11:54 +0200

    continue

--- utils/get_cool_date.py
+++ utils/get_cool_date.py
@@ -3,5 +3,9 @@ from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parents[1]/"setupsrc"))
 from base import get_cool_date
 
-n_days = int(sys.argv[1])
-print(get_cool_date(n_days))
+cooldown_days = int(sys.argv[1])
+freeze_date = sys.argv[2] if len(sys.argv) > 2 else ""
+if freeze_date:
+    print(freeze_date)
+else:
+    print(get_cool_date(cooldown_days))
