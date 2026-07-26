ad21784018a96655aaa9787a329929221eab23f7
geisserml <geisserml@gmail.com>
2026-07-26 23:13:14 +0200

    update_pip_cool: add hash checking via lockfiles

--- utils/update_pip_cool.py
+++ utils/update_pip_cool.py
@@ -7,7 +7,7 @@ import subprocess
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parents[1]/"setupsrc"))
-from simplebase import log, get_cool_date
+from simplebase import ProjectDir, log, get_cool_date
 
 def run(cmd, check=True, **kwargs):
     log(cmd)
@@ -25,18 +25,17 @@ log(f"pip version is {pip_version}")
 
 # try to obtain a pip version that honors cooldown before updating pip unbounded
 if pip_major < 26:
-    # TODO add hash checking
     py_version = sys.version_info[:2]
     log(f"Python version is {py_version}")
     if py_version == (3, 9):
-        pip_pin, update_ok = "26.0.1", True
+        pip_lock, update_ok = "py_39", True
     elif py_version == (3, 8):
         log("WARNING: Python 3.8's max pip does not support dependency cooldowns!")
-        pip_pin, update_ok = "25.0.1", False
+        pip_lock, update_ok = "py_38", False
     else:
         assert py_version >= (3, 10)
-        pip_pin, update_ok = "26.1.2", True
-    run([sys.executable, "-m", "pip", "install", f"pip=={pip_pin}"])
+        pip_lock, update_ok = "py_current", True
+    run([sys.executable, "-m", "pip", "install", "--require-hashes", "--no-deps", "-r", ProjectDir/"lock"/"pip"/f"{pip_lock}.txt"])
 else:
     update_ok = True
 
