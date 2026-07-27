94c1d34f1952a27c791e510eb208a52968039397
geisserml <geisserml@gmail.com>
2026-07-27 13:57:39 +0200

    update_pip_cool: limit to python >= 3.6

--- utils/update_pip_cool.py
+++ utils/update_pip_cool.py
@@ -28,23 +28,21 @@ log(f"pip version is {pip_version}")
 
 # Try to obtain a pip version that honors cooldown before updating pip unbounded
 # NOTE: We're always using lockfiles here, because for some reason pip supports --hash in requirements files earlier than it does on a normal pip install command.
-update_ok, pass_cooldown = True, True
+update_ok = True
 if pip_major < 26:
-    py_version = sys.version_info
+    py_version = sys.version_info[:2]
     log(f"Python version is {tuple(py_version)}")
     if py_version >= (3, 10):
         pip_lock = LockDir/"pip.txt"
     elif py_version >= (3, 6):
-        pip_lock = LockDir_Pip/f"py{py_version.major}{py_version.minor}.txt"
+        pip_lock = LockDir_Pip/f"py{py_version[0]}{py_version[1]}.txt"
         update_ok = py_version >= (3, 9)
         if not update_ok:
             log("WARNING: Max known pip version does not support dependency cooldown. Will not attempt to update pip further.")
     else:
-        pip_lock, pass_cooldown = None, False
-        log("WARNING: Unhandled python version below 3.6 - don't have a pip pin. Will proceed with unsafe update to the max pip version. Dependency cooldown will not be supported.")
+        raise ValueError("Unsupported Python version below 3.6 - don't know max pip version.")
     if pip_lock:
         run([sys.executable, "-m", "pip", "install", "--no-deps", "--require-hashes", "-r", str(pip_lock)])
 
 if update_ok:
-    cooldown_args = ("--uploaded-prior-to", get_cool_date(3)) if pass_cooldown else ()
-    run([sys.executable, "-m", "pip", "install", *cooldown_args, "-U", "pip"])
+    run([sys.executable, "-m", "pip", "install", "--uploaded-prior-to", get_cool_date(3), "-U", "pip"])
