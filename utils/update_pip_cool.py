a18a7b6967607b292b58b29d2b387050f851a4c3
geisserml <geisserml@gmail.com>
2026-07-27 09:50:33 +0200

    update_pip_cool: python < 3.8 compat fix

--- utils/update_pip_cool.py
+++ utils/update_pip_cool.py
@@ -27,18 +27,24 @@ log(f"pip version is {pip_version}")
 if pip_major < 26:
     py_version = sys.version_info[:2]
     log(f"Python version is {py_version}")
+    pass_cooldown = True
+    if py_version >= (3, 10):
+        pip_lock, update_ok = "py_current", True
     if py_version == (3, 9):
-        pip_lock, update_ok = "py_39", True
+        pip_lock, update_ok = "py_39", True  # supports cooldown
     elif py_version == (3, 8):
         log("WARNING: Python 3.8's max pip does not support dependency cooldowns!")
         pip_lock, update_ok = "py_38", False
     else:
-        assert py_version >= (3, 10)
-        pip_lock, update_ok = "py_current", True
-    run([sys.executable, "-m", "pip", "install", "--require-hashes", "--no-deps", "-r", ProjectDir/"lock"/"pip"/f"{pip_lock}.txt"])
+        # Python 3.6 max pip is 21.3.1. Not sure about Python 3.7.
+        log("WARNING: Unhandled python version below 3.8. Doing unsafe update to the max pip version. It will not support dependency cooldowns!")
+        pip_lock, update_ok = None, True
+        pass_cooldown = False
+    if pip_lock:
+        run([sys.executable, "-m", "pip", "install", "--require-hashes", "--no-deps", "-r", ProjectDir/"lock"/"pip"/f"{pip_lock}.txt"])
 else:
     update_ok = True
 
 if update_ok:
-    cool_date = get_cool_date(3)
-    run([sys.executable, "-m", "pip", "install", "--uploaded-prior-to", cool_date, "-U", "pip"])
+    cooldown_args = ("--uploaded-prior-to", get_cool_date(3)) if pass_cooldown else ()
+    run([sys.executable, "-m", "pip", "install", *cooldown_args, "-U", "pip"])
