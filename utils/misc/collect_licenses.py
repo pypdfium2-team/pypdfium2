35ea0c1687d92f5828d5f84316fddfa311975b03
geisserml <geisserml@gmail.com>
2026-08-02 19:12:21 +0200

    utils/misc: fix adding setupsrc/ to sys.path

--- utils/misc/collect_licenses.py
+++ utils/misc/collect_licenses.py
@@ -6,7 +6,7 @@ import shutil
 import argparse
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parents[1]/"setupsrc"))
+sys.path.insert(0, str(Path(__file__).parents[2]/"setupsrc"))
 from simplebase import ProjectDir
 
 DataDir = ProjectDir/"data"
--- utils/misc/get_cool_date.py
+++ utils/misc/get_cool_date.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-sys.path.insert(0, str(Path(__file__).parents[1]/"setupsrc"))
+sys.path.insert(0, str(Path(__file__).parents[2]/"setupsrc"))
 from simplebase import get_cool_date
 
 cooldown_days = int(sys.argv[1])
