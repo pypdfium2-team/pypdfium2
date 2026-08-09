85156e5eb924b35024cea0272f4adfebb25b12ca
geisserml <geisserml@gmail.com>
2026-08-09 23:06:13 +0200

    fix_venv.py: style nit: align `*_exe` variables

--- utils/misc/fix_venv.py
+++ utils/misc/fix_venv.py
@@ -8,10 +8,10 @@ assert VENV_ROOT.exists(), f"{VENV_ROOT} does not exist"
 assert VENV_BIN.exists(), f"{VENV_BIN} does not exist"
 
 if IS_WINDOWS:
-    python_exe = VENV_BIN/"python.exe"
-    pip_exe = VENV_BIN/"pip.exe"
+    python_exe  = VENV_BIN/"python.exe"
+    pip_exe     = VENV_BIN/"pip.exe"
     python3_exe = VENV_BIN/"python3.exe"
-    pip3_exe = VENV_BIN/"pip3.exe"
+    pip3_exe    = VENV_BIN/"pip3.exe"
     
     assert python_exe.exists(), "python.exe must exist"
     assert pip_exe.exists(), "pip.exe must exist"
