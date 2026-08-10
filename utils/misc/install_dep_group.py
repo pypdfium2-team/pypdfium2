23ad67d87745c25811b32a5bb86e8e1abdfa93b5
geisserml <geisserml@gmail.com>
2026-08-10 18:53:32 +0200

    continue

--- utils/misc/install_dep_group.py
+++ utils/misc/install_dep_group.py
@@ -3,4 +3,5 @@ from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parents[2]/"setupsrc"))
 from shared_base import install_dep_groups
 
+# TODO make need_fallback conditional (flag argument)
 install_dep_groups(sys.argv[1:], need_fallback=True)
