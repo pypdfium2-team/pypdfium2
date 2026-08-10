a16ae15bd9e9595848426857c41ab81aa9dca66e
geisserml <geisserml@gmail.com>
2026-08-10 19:13:39 +0200

    install_dep_group.py: make fallback conditional

--- utils/misc/install_dep_group.py
+++ utils/misc/install_dep_group.py
@@ -1,7 +1,12 @@
 import sys
+import argparse
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parents[2]/"setupsrc"))
-from shared_base import install_dep_groups
+from shared_base import install_dep_groups, _DEPGROUP_FALLBACK
 
-# TODO make need_fallback conditional (flag argument)
-install_dep_groups(sys.argv[1:], need_fallback=True)
+parser = argparse.ArgumentParser()
+parser.add_argument("groups", nargs="+")
+parser.add_argument("-f", "--fallback", action="store_true", default=_DEPGROUP_FALLBACK)
+args = parser.parse_args()
+
+install_dep_groups(args.groups, need_fallback=args.fallback)
