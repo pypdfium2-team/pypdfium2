3282e36375fd6e139f885ff634747ab0fe643e66
geisserml <geisserml@gmail.com>
2026-08-12 13:05:39 +0200

    Add zizmor to check step

--- utils/misc/check.sh
+++ utils/misc/check.sh
@@ -4,6 +4,7 @@ set -exuo pipefail
 AUTOFLAKE_DIFF=$(autoflake src/ setupsrc/ tests/ setup.py docs/source/conf.py --recursive --remove-all-unused-imports --ignore-pass-statements --ignore-init-module-imports)
 codespell --skip="./docs/build,./tests/resources,./tests/output,./data,./sbuild,./patches,./dist,./LICENSES,./BUILD_LICENSES,./RELEASE.md,./.git,./htmlcov,__pycache__,.mypy_cache,.hypothesis" -L "FitH,flate,intoto"
 reuse lint
+zizmor .github/ --persona auditor
 
 if [[ -n "$AUTOFLAKE_DIFF" ]]; then
   exit 1
